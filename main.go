20c2c4c4172c9fd8309e1907f3af7e2c6f797465
mikeathan <mikeathan@gmail.com>
2023-05-09 20:26:15 +0100

    cleanup

--- main.go
+++ main.go
@@ -6,30 +6,6 @@ import (
 	"net/http"
 )
 
-type Todo struct {
-	Title string
-	Done  bool
-}
-
-type TodoPageData struct {
-	PageTitle string
-	Todos     []Todo
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
-
-	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintf(w, "Hello!")
-}
-
 func main() {
 
 	fileServer := http.FileServer(http.Dir("./frontend/dist"))
