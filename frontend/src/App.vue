66f81e63aab46815bf2191ae3939e15e63ec83fd
mikeathan <mikeathan@gmail.com>
2023-05-09 17:07:04 +0100

    wip

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,32 @@
-<template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref } from 'vue'
+import SensorData from './components/SensorData.vue'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const message = ref('Node-Herder Page')
+const deviceData = ref([{
+    "battery": 100,
+    "humidity": 69.8,
+    "last_seen": "2023-05-06T19:13:37+01:00",
+    "linkquality": 29,
+    "temperature": 20,
+    "voltage": 3000
+}]);
+const groceryList = ref([
+  { id: 0, note: 'Vegetables' },
+  { id: 1, note: 'Cheese' },
+  { id: 2, note: 'Whatever else humans are supposed to eat' }
+])
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <h1>{{ message }}</h1>
+  <div>{{ deviceData }}</div>
+  <ol>
+  <SensorData
+  v-for="item in groceryList"
+      :device="item"
+      :key="item.id"
+    ></SensorData>
+  </ol>
+</template>
+
---
+++ frontend/src/components/SensorData.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps({
+  device: Object
+})
+</script>
+
+<template>
+  <ol>{{ device.note }}</ol>
+</template>
