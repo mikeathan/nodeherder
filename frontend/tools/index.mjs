0a31cf15c86ebd9c0a2a7f38a991b2ab7e9412b3
mikeathan <mikeathan@gmail.com>
2023-06-07 19:45:05 +0100

    mock values

--- frontend/tools/index.mjs
+++ frontend/tools/index.mjs
@@ -1,14 +1,19 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-import moment from 'moment'
-import 'moment-timezone'
+import path from "path";
+import { fileURLToPath } from "url";
+import moment from "moment";
+import "moment-timezone";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import express from 'express'
-import expressWs from 'express-ws'
-import http from 'http'
+import express from "express";
+import expressWs from "express-ws";
+import http from "http";
+
+// temperature
+const temperatureChangeDelaySec = 5;
+const temperatureMin = 10.0;
+const temperatureMax = 40.0;
 
 // Our port
 let port = 3000;
@@ -19,63 +24,103 @@ let app = express();
 let server = http.createServer(app).listen(port);
 console.log("[" + currentTime() + "] server listening at port " + port);
 
-let data = [{
-    "name": "device 1",
-    "payload": ""
-}, {
-    "name": "device 2",
-    "payload": ""
-}]
+let devicesConfig = [
+  {
+    name: "device 1",
+    temperatureOffset: 0.6,
+    humidityOffset: 11.3,
+    delayInSec: 20,
+    temperature: temperatureMin,
+    lastChanged: moment(),
+  },
+  {
+    name: "device 2",
+    temperatureOffset: 1.1,
+    humidityOffset: 9.7,
+    delayInSec: 30,
+    temperature: temperatureMin,
+    lastChanged: moment(),
+  },
+];
 
 expressWs(app, server);
 
 // Get the /ws websocket route
-app.ws('/ws', async function (ws, req) {
-    console.log("connected");
-    data.forEach((device) => {
+app.ws("/ws", async function (ws, req) {
+  console.log("client connected");
 
-        var p = mockTHDevicePayload("connected");
-       // console.log(p);
-        device.payload = p
-        ws.send(JSON.stringify(device));
+  devicesConfig.forEach((config) => {
+    var device = buildPayload("connected", config);
+    ws.send(device);
+  });
+
+  setInterval(function () {
+    counter++;
+
+    // todo kill timer if client is disconnected
+    devicesConfig.forEach((device) => {
+      devicesConfig.forEach((config) => {
+        var device = buildPayload("updated", config);
+        ws.send(device);
+      });
     });
+  }, 5000);
 
-    setInterval(function () {
-
-        counter++
-        // todo kill timer if client is disconnected
-        console.log("updated");
-
-        data.forEach((device) => {
-            
-            var p = mockTHDevicePayload("updated");
-            device.payload = p
-            ws.send(JSON.stringify(device));
-        });
-    }, 5000);
-
-
-    ws.on('message', async function (msg) {
-        console.log("message received" + msg);
-    });
+  ws.on("message", async function (msg) {
+    console.log("message received" + msg);
+  });
 });
 
+function buildPayload(status, settings) {
+  var data = {
+    name: settings.name,
+    payload: mockTHDevicePayload(status, settings),
+  };
+  var device = JSON.stringify(data);
+  //   console.log(
+  //     status +
+  //       "=>" +
+  //       data.name +
+  //       " temp: " +
+  //       data.payload.temperature +
+  //       " temp offset: " +
+  //       settings.temperatureOffset
+  //   );
+
+  return device;
+}
 function currentTime() {
-    var isoNow = moment().tz('Europe/London')
-    return isoNow.format();
+  var isoNow = moment().tz("Europe/London");
+  return isoNow.format();
 }
 
-function mockTHDevicePayload(state) {
-    var device = {
-        state: state + "=" + counter,
-        battery: 100,
-        humidity: 60.1,
-        last_seen: currentTime(),
-        linkquality: 47,
-        temperature: 19.1,
-        voltage: 3000
-    };   
+function mockTHDevicePayload(state, settings) {
+  var device = {
+    state: state + "=" + counter,
+    battery: 100,
+    humidity: 60.1,
+    last_seen: currentTime(),
+    linkquality: 47,
+    temperature: getMockTemperature(settings),
+    voltage: 3000,
+  };
 
-    return device;
+  return device;
 }
 
+function getMockTemperature(settings) {
+  var diff = moment().diff(settings.lastChanged);
+  var duration = moment.duration(diff);
+
+  if (duration.seconds() < temperatureChangeDelaySec) {
+    return settings.temperature;
+  }
+
+  if (settings.temperature > temperatureMax) {
+    settings.temperature = temperatureMin;
+  }
+
+  settings.temperature += settings.temperatureOffset;
+  settings.lastChanged = moment();
+  return settings.temperature;
+}
