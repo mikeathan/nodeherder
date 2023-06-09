a50ea4d14427d89d6a5581ffadf53b895edcad1b
mikeathan <mikeathan@gmail.com>
2023-06-09 19:30:26 +0100

    add footer component

---
+++ frontend/src/components/DeviceFooter.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {
+  formatLastSeen,
+  getBatteryIcon,
+  formatLinkQuality,
+  getLinkQualityIcon,
+} from "../modules/sensors/device";
+
+const props = defineProps({
+  payload: Object,
+});
+</script>
+
+<template>
+  <div class="card-text">
+    <span style="margin-right: 2.5rem">
+      {{ formatLastSeen(payload) }}
+    </span>
+    <span
+      :class="`fa fa-fw ${getLinkQualityIcon()}`"
+      style="margin-right: 0.25rem !important"
+    ></span>
+    <span style="margin-right: 0.5rem">
+      {{ formatLinkQuality(payload) }}
+    </span>
+    <span :class="`fa fa-fw ${getBatteryIcon(payload)}`"></span>
+  </div>
+</template>
