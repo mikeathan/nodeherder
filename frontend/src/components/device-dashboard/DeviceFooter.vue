a096404f067ee8f1a82837701987e158aff587c7
mikeathan <mikeathan@gmail.com>
2023-06-10 20:19:24 +0100

    use card footer

--- frontend/src/components/device-dashboard/DeviceFooter.vue
+++ frontend/src/components/device-dashboard/DeviceFooter.vue
@@ -12,11 +12,12 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="card-text">
-    <span style="margin-left: 0.5rem;margin-right: 3.5rem">
+  <p></p>
+  <div class="card-footer border-0">
+    <span style="margin-right: 3.1rem">
       {{ formatLastSeen(payload) }}
     </span>
-    <span :class="`fa fa-fw ${getLinkQualityIcon()}`" style="margin-left: 0.54rem;margin-right: 0.25rem;"></span>
+    <span :class="`fa fa-fw ${getLinkQualityIcon()}`" style="margin-left: 0.5rem;margin-right: 0.25rem;"></span>
     <span style="margin-right: 0.5rem">
       {{ formatLinkQuality(payload) }}
     </span>
