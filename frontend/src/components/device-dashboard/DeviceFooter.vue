1c5e526c6450d71d8a76c85835bac92897904945
mikeathan <mikeathan@gmail.com>
2023-06-11 20:15:39 +0100

    add on mounted

--- frontend/src/components/device-dashboard/DeviceFooter.vue
+++ frontend/src/components/device-dashboard/DeviceFooter.vue
@@ -1,25 +1,36 @@
 <script setup>
+import { onMounted } from "vue";
+import "../../assets/css/device.styles.css";
 import {
   formatLastSeen,
   getBatteryIcon,
   formatLinkQuality,
   getLinkQualityIcon,
 } from "../../modules/sensors/device";
-import "../../assets/css/device.styles.css";
+
 const props = defineProps({
   payload: Object,
 });
+
+
+
+onMounted(() => {
+  console.log("device footer mounted");
+
+});
+var lastSeenTimerId = undefined;
+function lastSeen(payload) {
+  return formatLastSeen(payload)
+};
+
 </script>
 
 <template>
   <div class="card-text">
     <span style="margin-right: 4.5rem">
-      {{ formatLastSeen(payload) }}
+      {{ lastSeen(payload) }}
     </span>
-    <span
-      :class="`fa fa-fw ${getLinkQualityIcon()}`"
-      class="device-info-span"
-    ></span>
+    <span :class="`fa fa-fw ${getLinkQualityIcon()}`" class="device-info-span"></span>
     <span class="device-info-span">
       {{ formatLinkQuality(payload) }}
     </span>
