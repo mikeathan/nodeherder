565672ef64abbe5f42586f1fd646a503c2e978f1
mikeathan <mikeathan@gmail.com>
2023-06-12 20:06:05 +0100

    padding

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
 import { ref } from "vue";
 import Dashboard from "./components/device-dashboard/DeviceDashboard.vue";
-const title = ref("Node-Herder Page");
+const title = ref("Node-Herder");
 </script>
+<style>
+div {
+  padding-left: 10px;
+}
+</style>
 
 <template>
   <h1>{{ title }}</h1>
