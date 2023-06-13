b29ce5aba5a5885381f4514588c2e8b26a878572
mikeathan <mikeathan@gmail.com>
2023-06-13 18:19:06 +0100

    add payload formatter

--- frontend/src/components/device-dashboard/DeviceFooter.vue
+++ frontend/src/components/device-dashboard/DeviceFooter.vue
@@ -1,52 +1,59 @@
 <script setup>
-import { watch, ref, onMounted } from "vue";
+import { watch, ref, onMounted, onUnmounted } from "vue";
 import "../../assets/css/device.styles.css";
-import {
-  formatLastSeen,
-  getBatteryIcon,
-  formatLinkQuality,
-  getLinkQualityIcon,
-} from "../../modules/sensors/device";
-
+import PayloadFormatter from "../../modules/payload-formatter";
 const props = defineProps({
   payload: Object,
 });
 
 var lastSeenTimerId = undefined;
 let lastSeenUpdater = ref("");
-
+let payloadFormatter = ref(PayloadFormatter);
 watch(
   () => props.payload,
   (newpayload) => {
     console.log("Watch props.payload update " + newpayload.name);
-
-    if (lastSeenTimerId != undefined) {
-      clearInterval(lastSeenTimerId);
-      console.log("clearInterval " + lastSeenTimerId);
-    }
-    lastSeenTimerId = setInterval(function () {
-      lastSeenUpdater.value = formatLastSeen(newpayload);
-    }, 1000);
-
-    console.log("setInterval " + newpayload.name + " id: " + lastSeenTimerId);
+    payloadFormatter.update(newpayload);
   }
 );
 
 onMounted(() => {
-  lastSeenUpdater.value = formatLastSeen(props.payload);
+  console.log("mounted: " + props.payload.name);
+  payloadFormatter = new PayloadFormatter();
+  payloadFormatter.update(props.payload);
 });
-
+onUnmounted(() => {
+  console.log("unmounted: " + props.payload.name);
+  payloadFormatter.dispose();
+});
 </script>
 
 <template>
   <div class="card-text">
     <span style="margin-right: 4.5rem">
+      {{ payloadFormatter.lastSeen }}
+    </span>
+    <span
+      :class="`fa fa-fw ${payloadFormatter.linkQualityIconClass}`"
+      class="device-info-span"
+    ></span>
+    <span class="device-info-span">
+      {{ payloadFormatter.linkQuality }}
+    </span>
+    <span :class="`fa fa-fw ${payloadFormatter.batteryIconClass}`"></span>
+  </div>
+
+  <!-- <div class="card-text">
+    <span style="margin-right: 4.5rem">
       {{ lastSeenUpdater }}
     </span>
-    <span :class="`fa fa-fw ${getLinkQualityIcon()}`" class="device-info-span"></span>
+    <span
+      :class="`fa fa-fw ${getLinkQualityIcon()}`"
+      class="device-info-span"
+    ></span>
     <span class="device-info-span">
       {{ formatLinkQuality(payload) }}
     </span>
     <span :class="`fa fa-fw ${getBatteryIcon(payload)}`"></span>
-  </div>
+  </div> -->
 </template>
---
+++ frontend/src/modules/payload-formatter.js
@@ -0,0 +1,119 @@
+import moment from "moment";
+import "moment-timezone";
+import { isProxy, toRaw } from "vue";
+
+export default class PayloadFormatter {
+  constructor() {}
+
+  startLastSeenTimer(payload) {
+    this.stoplastSeenTimer();
+    this.lastSeenTimerId = setInterval(function () {
+      this.lastSeen = formatLastSeen(payload);
+    }, 1000);
+
+    console.log("setInterval " + payload.name + " id: " + this.lastSeenTimerId);
+  }
+
+  stoplastSeenTimer() {
+    if (this.lastSeenTimerId != undefined) {
+      clearInterval(this.lastSeenTimerId);
+      console.log("clearInterval " + this.lastSeenTimerId);
+    }
+  }
+
+  dispose() {
+    this.stoplastSeenTime();
+  }
+
+  update(payload) {
+    if (isProxy(payload)) {
+      payload = toRaw(payload);
+    }
+
+    this.lastSeen = formatLastSeen(payload);
+    this.linkQuality = formatLinkQuality(payload);
+    this.batteryIconClass = getBatteryIcon(payload);
+    this.linkQualityIconClass = getLinkQualityIcon(payload);
+
+    this.startLastSeenTimer(payload);
+  }
+}
+
+function formatLastSeen(payload) {
+  if (isProxy(payload)) {
+    payload = toRaw(payload);
+  }
+
+  if (payload["last_seen"] == undefined) {
+    return "";
+  }
+
+  var sensorLastSeen = payload["last_seen"];
+
+  var lastSeen = moment(sensorLastSeen);
+  var diff = moment().diff(lastSeen);
+  var duration = moment.duration(diff);
+
+  var formatted = "just now";
+  // TODO: handle days() > 0
+  if (duration.hours() > 0) {
+    formatted = duration.hours() + " hours ago";
+  }
+  if (duration.minutes() > 0) {
+    formatted = duration.minutes() + " minutes ago";
+  }
+  if (duration.seconds() > 5) {
+    formatted = duration.seconds() + " seconds ago";
+  }
+
+  return formatted;
+}
+
+function getBatteryIcon(payload) {
+  if (isProxy(payload)) {
+    payload = toRaw(payload);
+  }
+
+  if (payload["battery"] == undefined) {
+    return "";
+  }
+
+  var batteryClass = "";
+  var battery = payload["battery"];
+  if (battery >= 85) {
+    batteryClass += " fa-battery-full";
+  } else if (battery >= 75) {
+    batteryClass += " fa-battery-three-quarters";
+  } else if (battery >= 50) {
+    batteryClass += " fa-battery-half";
+  } else if (battery >= 25) {
+    batteryClass += " fa-battery-quarter";
+  } else if (battery >= 10) {
+    batteryClass += ` fa-battery-empty animation-blinking`;
+  } else {
+    return `animation-blinking text-danger`;
+  }
+
+  /* // caption
+  // var title = `${battery ? `, power_level` + ` ${battery}%` : ""}`; */
+  if (!batteryClass) {
+    batteryClass = "fa-question";
+  }
+  return batteryClass;
+}
+function getLinkQualityIcon() {
+  return "fa-signal fa-fw";
+}
+
+function formatLinkQuality(payload) {
+  if (isProxy(payload)) {
+    payload = toRaw(payload);
+  }
+  if (payload["linkquality"] == undefined) {
+    return "";
+  }
+
+  var linkQuality = payload["linkquality"];
+
+  return linkQuality + " LQI";
+}
