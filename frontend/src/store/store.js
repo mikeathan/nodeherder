9e97abd7f5fbbbceb70b74a789323d814d3b9d61
mikeathan <mikeathan@gmail.com>
2023-06-13 18:19:17 +0100

    delete

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore } from "vuex";
 import createPersistedState from "vuex-persistedstate";
-// import { Device } from "../modules/sensors/device";
+
 const state = {
   devices: {},
 };
