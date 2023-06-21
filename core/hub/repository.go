12d8d46e652aecddf22862d1595e20bf137fe97b
mikeathan <mikeathan@gmail.com>
2023-06-21 17:58:33 +0100

    implement repository

--- core/hub/repository.go
+++ core/hub/repository.go
@@ -1,23 +1,49 @@
 package hub
 
+import "sort"
+
+type Device struct {
+	Name    string      `json:"name"`
+	Payload interface{} `json:"payload"`
+}
+
+func newDevice(name string, payload interface{}) *Device {
+	return &Device{Name: name, Payload: payload}
+}
+
 type Repository interface {
-	Add(deviceName string, payload interface{})
-	ListAll() interface{}
+	AddDevice(deviceName string, payload interface{})
+	ListAllDevices() []*Device
 }
 
 type MemoryRepository struct {
-	store map[string]interface{}
+	store map[string]*Device
 }
 
 func NewMemoryRepository() Repository {
-	return &MemoryRepository{store: map[string]interface{}{}}
+	return &MemoryRepository{store: map[string]*Device{}}
 }
 
-func (s *MemoryRepository) Add(deviceName string, payload interface{}) {
+func (s *MemoryRepository) AddDevice(deviceName string, payload interface{}) {
 
-	s.store[deviceName] = payload
+	if _, ok := s.store[deviceName]; !ok {
+		s.store[deviceName] = newDevice(deviceName, payload)
+	}
+	s.store[deviceName].Payload = payload
 }
 
-func (s *MemoryRepository) ListAll() interface{} {
-	return nil
+func (s *MemoryRepository) ListAllDevices() []*Device {
+
+	// sort before returning values
+	keys := make([]string, 0, len(s.store))
+	for k := range s.store {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	devices := make([]*Device, 0, len(s.store))
+	for _, key := range keys {
+		devices = append(devices, s.store[key])
+	}
+	return devices
 }
---
+++ core/hub/repository_test.go
@@ -0,0 +1,80 @@
+package hub_test
+
+import (
+	"node-herder/hub"
+	"testing"
+)
+
+const data1 = "{'battery':100,'humidity':59.8,'last_seen':'2023-05-31T19:02:28+01:00','linkquality':51,'temperature':18.4,'voltage':3000}"
+
+const data2 = "{'battery':95,'humidity':60.8,'last_seen':'2023-05-31T19:05:28+01:00','linkquality':50,'temperature':22.1,'voltage':3000}"
+
+func TestRepositoryCanAddOneDevice(t *testing.T) {
+
+	repo := hub.NewMemoryRepository()
+	repo.AddDevice("TH1", data1)
+
+	devices := repo.ListAllDevices()
+
+	if len(devices) == 0 {
+		t.Errorf("empty device list")
+	}
+	if len(devices) > 1 {
+		t.Errorf("contains invalid mismatch")
+	}
+	if devices[0].Payload != data1 {
+		t.Errorf("device payload mismatch ")
+	}
+	if devices[0].Name != "TH1" {
+		t.Errorf("device name mismatch ")
+	}
+}
+
+func TestRepositoryCanAddMultipleDevices(t *testing.T) {
+
+	repo := hub.NewMemoryRepository()
+	repo.AddDevice("TH1", data1)
+	repo.AddDevice("TH2", data2)
+	devices := repo.ListAllDevices()
+
+	if len(devices) == 0 {
+		t.Errorf("empty device list")
+	}
+	if len(devices) > 2 {
+		t.Errorf("contains invalid devices")
+	}
+	if devices[0].Payload != data1 {
+		t.Errorf("device 1 payload mismatc ")
+	}
+	if devices[1].Payload != data2 {
+		t.Errorf("device 2 payload mismatch ")
+	}
+	if devices[0].Name != "TH1" {
+		t.Errorf("device 1 name mismatch ")
+	}
+	if devices[1].Name != "TH2" {
+		t.Errorf("device 2 name mismatch ")
+	}
+}
+
+func TestRepositoryCanUpdateExistingDevice(t *testing.T) {
+
+	repo := hub.NewMemoryRepository()
+	repo.AddDevice("TH1", data1)
+	repo.AddDevice("TH1", data2)
+	devices := repo.ListAllDevices()
+
+	if len(devices) == 0 {
+		t.Errorf("empty device list")
+	}
+	if len(devices) > 1 {
+		t.Errorf("contains invalid devices")
+	}
+	if devices[0].Payload != data2 {
+		t.Errorf("device payload mismatch")
+	}
+
+	if devices[0].Name != "TH1" {
+		t.Errorf("device name mismatc ")
+	}
+}
