be7a4413dbb20e7fd7db710b1ae67ddd79df2e20
mikeathan <mikeathan@gmail.com>
2023-07-09 11:42:35 +0100

    add wip components

---
+++ frontend/src/components/device/LinkQuality.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps({
+  value: Number,
+});
+</script>
+<template>
+  <span key="linkquality" className="me-1">
+    <i :class="`fa fa-fw fa-signal fa-fw`"></i>
+    {{ value + " LQI" }}
+  </span>
+</template>
---
+++ frontend/src/components/device/PowerSource.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { getPowerSourceIcon } from "../../modules/sensors/device-formatter";
+const props = defineProps({
+  value: Number,
+});
+</script>
+<template>
+  <span :class="`fa fa-fw ${getPowerSourceIcon(value)}`"></span>
+</template>
