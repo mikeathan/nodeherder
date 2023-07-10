26074085c87ccbfc55c2070182ec7627b6d646fa
mikeathan <mikeathan@gmail.com>
2023-07-10 20:29:16 +0100

    wip close button

--- frontend/src/components/device-dashboard/DeviceCard.vue
+++ frontend/src/components/device-dashboard/DeviceCard.vue
@@ -6,10 +6,17 @@ import Sensor from "../device/Sensor.vue";
 const props = defineProps({
   device: Object,
 });
+// $('.close-icon').on('click', function () {
+//   $(this).closest('.card').fadeOut();
+// })
 </script>
 <template>
   <div class="col-xl-3 col-lg-4 col-sm-6 col-12 d-flex">
     <div class="card flex-fill flex-shrink-1">
+
+      <span class="pull-right clickable close-icon" data-effect="fadeOut"><i class="fa fa-times"></i></span>
+
+
       <div class="card-header pb-0 d-flex justify-content-between">
         {{ device.id }}
         <Availability :status="device.stats.availability" />
