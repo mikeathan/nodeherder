9aaee938dedbbc285b648ad8c607f10af2e3fb28
mikeathan <mikeathan@gmail.com>
2023-07-11 19:43:39 +0100

    device page wip

--- frontend/src/components/device-dashboard/DeviceCard.vue
+++ frontend/src/components/device-dashboard/DeviceCard.vue
@@ -6,24 +6,20 @@ import Sensor from "../device/Sensor.vue";
 const props = defineProps({
   device: Object,
 });
-// $('.close-icon').on('click', function () {
-//   $(this).closest('.card').fadeOut();
-// })
 </script>
 <template>
   <div class="col-xl-3 col-lg-4 col-sm-6 col-12 d-flex">
     <div class="card flex-fill flex-shrink-1">
-
-      <span class="pull-right clickable close-icon" data-effect="fadeOut"><i class="fa fa-times"></i></span>
-
-
       <div class="card-header pb-0 d-flex justify-content-between">
-        {{ device.id }}
+        <RouterLink :to="'/device/devicepage'">{{ device.id }}</RouterLink>
+
         <Availability :status="device.stats.availability" />
       </div>
-
       <div class="card-body row align-items-center">
-        <div class="d-flex align-items-center" v-for="(value, sensor) in device.sensors">
+        <div
+          class="d-flex align-items-center"
+          v-for="(value, sensor) in device.sensors"
+        >
           <Sensor :name="sensor" :value="value" />
         </div>
       </div>
---
+++ frontend/src/components/device/DevicePage.vue
@@ -0,0 +1,51 @@
+<script setup>
+const props = defineProps({
+  device: Object,
+});
+const displayProps = [
+  {
+    translationKey: "Friendly name",
+    render: () => (
+      <dd className="col-12 col-md-7">
+        <strong>{device.id}</strong>
+      </dd>
+    ),
+    translationKey: "Connection Type",
+    render: () => (
+      <dd className="col-12 col-md-7">
+        <strong>{device.conn}</strong>
+      </dd>
+    ),
+    translationKey: "Last seen",
+    render: () => (
+      <dd className="col-12 col-md-7">
+        <strong> WIP</strong>
+      </dd>
+    ),
+    translationKey: "Availability",
+    render: () => (
+      <dd className="col-12 col-md-7">
+        <strong> WIP</strong>
+      </dd>
+    ),
+    translationKey: "Availability",
+    render: () => (
+      <dd className="col-12 col-md-7">
+        <strong> WIP</strong>
+      </dd>
+    ),
+    translationKey: "Power source",
+    render: () => (
+      <dd className="col-12 col-md-7">
+        <strong> WIP</strong>
+      </dd>
+    ),
+  },
+];
+</script>
+<template>
+  <dl className="row" v-for="(key, value) in displayProps">
+    <dt className="col-12 col-md-5">{key}</dt>
+  </dl>
+  <!-- add device control -->
+</template>
