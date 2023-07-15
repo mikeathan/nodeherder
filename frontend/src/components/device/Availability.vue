dc105f74e8a6f826d759400576564af6cb7f5dd2
mikeathan <mikeathan@gmail.com>
2023-07-15 19:55:16 +0100

    test availaility dot

--- frontend/src/components/device/Availability.vue
+++ frontend/src/components/device/Availability.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { ref } from 'vue';
+
 const props = defineProps({
   status: String,
+  showAnimation: ref(true)
 });
 </script>
 <template>
-  <small key="Availability" v-if="status === 'offline'">
+  <i class="fa fa-circle" style="color: green"></i>
+  <div key="Availability" v-if="status === 'offline'">
     <i class="fa-solid text-danger fa-triangle-exclamation fa-fade"></i>
     Offline
-  </small>
+  </div>
 </template>
