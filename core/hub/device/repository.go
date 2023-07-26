68b818391505acbb8ab6671061a02756ec503228
mikeathan <mikeathan@gmail.com>
2023-07-26 10:57:50 +0100

    add mutex in repo

--- core/hub/device/repository.go
+++ core/hub/device/repository.go
@@ -3,6 +3,7 @@ package device
 import (
 	"errors"
 	"sort"
+	"sync"
 )
 
 type Repository interface {
@@ -13,18 +14,23 @@ type Repository interface {
 
 type MemoryNodeRepository struct {
 	store map[string]*NodePayload
+	mutex sync.RWMutex
 }
 
 func NewMemoryNodeRepository() Repository {
-	return &MemoryNodeRepository{store: map[string]*NodePayload{}}
+	return &MemoryNodeRepository{store: map[string]*NodePayload{}, mutex: sync.RWMutex{}}
 }
 
 func (s *MemoryNodeRepository) Store(deviceName string, payload *NodePayload) {
+	s.mutex.Lock()
 	s.store[deviceName] = payload
+	s.mutex.Unlock()
 }
 
 func (s *MemoryNodeRepository) FindDevice(deviceName string) (*NodePayload, error) {
+	defer s.mutex.RUnlock()
 
+	s.mutex.RLock()
 	if val, ok := s.store[deviceName]; ok {
 		return val, nil
 	}
@@ -36,10 +42,12 @@ func (s *MemoryNodeRepository) FindDevice(deviceName string) (*NodePayload, erro
 func (s *MemoryNodeRepository) ListAllDevices() []*NodePayload {
 
 	// sort before returning values
+	s.mutex.RLock()
 	keys := make([]string, 0, len(s.store))
 	for k := range s.store {
 		keys = append(keys, k)
 	}
+
 	sort.Strings(keys)
 
 	devices := make([]*NodePayload, 0, len(s.store))
@@ -47,5 +55,7 @@ func (s *MemoryNodeRepository) ListAllDevices() []*NodePayload {
 		device := s.store[key]
 		devices = append(devices, device)
 	}
+	s.mutex.RUnlock()
+
 	return devices
 }
