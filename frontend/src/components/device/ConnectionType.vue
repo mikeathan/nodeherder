b0cbefa53656ad826b813443f59fe8aef0deac04
mikeathan <mikeathan@gmail.com>
2023-07-31 16:59:44 +0100

    cleanup images

--- frontend/src/components/device/ConnectionType.vue
+++ frontend/src/components/device/ConnectionType.vue
@@ -1,18 +1,18 @@
 <script setup>
-import MqqtImage from "../../../../assets/mqttorg-graphics-master/png/mqtt-hor-neg.png"
-import HttpImage from "../../../../assets/HTTP_logo.svg.png"
+import MqqtImage from "../assets/images/transport/mqtt-logo.png";
+import HttpImage from "../assets/images/transport/http-logo.png";
 const props = defineProps({
-    type: String,
+  type: String,
 });
 </script>
 <template>
-    <div title="ConnectionType" v-if="type === 'mqtt'">
-        <img :src="MqqtImage" width="100" />
-    </div>
-    <div title="ConnectionType" v-else-if="type === 'http'">
-        <img :src="HttpImage" width="80" />
-    </div>
-    <div title="ConnectionType" v-else>
-        {{ type }}
-    </div>
+  <div title="ConnectionType" v-if="type === 'mqtt'">
+    <img :src="MqqtImage" width="100" />
+  </div>
+  <div title="ConnectionType" v-else-if="type === 'http'">
+    <img :src="HttpImage" width="80" />
+  </div>
+  <div title="ConnectionType" v-else>
+    {{ type }}
+  </div>
 </template>
