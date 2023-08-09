129a71d43bdfa0784ed462e255c5baaa32efb8c5
mikeathan <mikeathan@gmail.com>
2023-08-09 20:24:33 +0100

    automation wup

--- core/automation/tiggers.go
+++ core/automation/tiggers.go
@@ -1,19 +1,43 @@
 package automation
 
-import "errors"
+import (
+	"fmt"
+	"time"
+)
 
 type Trigger interface {
-	Trigger() error
 	Name() string
+	WithCondition(cond string)
+	WithAction(action string)
+	Process() error
+	Trigger() error
 }
 
 type TimerTrigger struct {
+	cond   string
+	action string
 }
 
 func (t TimerTrigger) Trigger() error {
-	return errors.New("not implemented")
+
+	fmt.Printf("Trigger Action: %s\n", t.action)
+	return nil
 }
 
 func (t TimerTrigger) Name() string {
 	return "Timer"
+}
+
+func (t *TimerTrigger) WithCondition(cond string) {
+	t.cond = cond
+}
+func (t *TimerTrigger) WithAction(action string) {
+	t.action = action
+}
+
+func (t *TimerTrigger) Process() error {
+	ticker := *time.NewTicker(1 * time.Second)
+	<-ticker.C
+
+	return t.Trigger()
 }
--- core/automation/triggers_test.go
+++ core/automation/triggers_test.go
@@ -7,12 +7,23 @@ import (
 
 func TestTriggers(t *testing.T) {
 
-	trigger := new(automation.TimerTrigger)
-	err := trigger.Trigger()
+	// payload coming in
+	// if name matches our list of triggers
+	// then
+	// find trigger sensor forthat device
+	// and process value
 
-	if err != nil {
-		t.Fatalf("error %v", err.Error())
-	}
+	// BUT that doesn work with timer
+	// input is not payload but is timer
+	// .....
+	// maybe timer does not go through same flow
+	// we need automation processor
+	//......
+
+	trigger := new(automation.TimerTrigger)
+	trigger.WithCondition("some condition")
+	trigger.WithAction("some action")
+	trigger.Process()
 
 	// https://www.home-assistant.io/docs/automation/basics/
 	// https://www.home-assistant.io/docs/automation/editor/
