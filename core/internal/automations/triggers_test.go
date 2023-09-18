a796232557cfdc532c10e74943381b8485f17319
mikeathan <mikeathan@gmail.com>
2023-09-18 09:04:58 +0100

    enable test

--- core/internal/automations/triggers_test.go
+++ core/internal/automations/triggers_test.go
@@ -262,12 +262,14 @@ func TestOneConditionWithSensorConstraintAndOneConditionWithTimerConstraint(t *t
 		result    bool
 	}{
 		{occupancy: true, luxValue: 35, result: false},
-		{occupancy: true, luxValue: 30, result: true}, // turn on light
-		//{occupancy: false, luxValue: 100, result: true}, // turn off light
-		//	{occupancy: true, luxValue: 29.9, result: true}, // turn on light
-		// {occupancy: true, luxValue: 31, result: false},
-		// {occupancy: true, luxValue: 100, result: false},
-		// {occupancy: true, luxValue: 10, result: true},   // turn on light
+		{occupancy: true, luxValue: 30, result: true},   // turn on light
+		{occupancy: false, luxValue: 100, result: true}, // turn off light
+		{occupancy: true, luxValue: 29.9, result: true}, // turn on light
+		{occupancy: true, luxValue: 31, result: false},
+		{occupancy: true, luxValue: 100, result: false},
+		{occupancy: true, luxValue: 10, result: true},  // turn on light
+		{occupancy: true, luxValue: 10, result: false}, // turn on light - result should fail
+
 		// {occupancy: false, luxValue: 10, result: false}, // turn off light
 		//	{occupancy: true, luxValue: 10, result: false}, // turn on light - result should fail
 		//	{occupancy: true, luxValue: 10, result: false}, // turn on light -  result should fail
