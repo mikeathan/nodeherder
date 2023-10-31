453d67f2eec365f48813ec60e1d57783f43daf8c
mikeathan <mikeathan@gmail.com>
2023-10-31 18:17:47 +0000

    comment debug print

--- core/internal/ws/ws.go
+++ core/internal/ws/ws.go
@@ -196,7 +196,7 @@ func (c *WsClient) Broadcast(eventName string, data interface{}) error {
 	if err != nil {
 		return errors.New("failed to marshal client payload")
 	}
-	fmt.Print(string(bytes))
+	//fmt.Print(string(bytes))
 	c.send <- bytes
 	return nil
 }
