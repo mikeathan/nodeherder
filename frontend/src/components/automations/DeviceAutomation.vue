bff11b9416d635c164859a8ec2586deababd2f5d
mikeathan <mikeathan@gmail.com>
2023-11-14 06:59:45 +0000

    create new device trigger

--- frontend/src/components/automations/DeviceAutomation.vue
+++ frontend/src/components/automations/DeviceAutomation.vue
@@ -13,10 +13,11 @@ const props = defineProps({
 });
 
 const store = useStore();
-const deviceTrigger = ref(new DeviceTrigger())
 const conditions = ref([]);
 const selectedExpose = ref("")
-const exposeTriggers = ref({});
+const exposeTriggers = {};
+
+const description = ref("")
 const device = computed(() => {
     return store.getters["devices/find"](props.id);
 });
@@ -45,22 +46,10 @@ function exposeSelectionChanged(event) {
 watch(
     () => props.id,
     (newId) => {
-        deviceTrigger.value = new DeviceTrigger()
-        deviceTrigger.value.id = newId
         selectedExpose.value = null
     },
     { immediate: true }
 );
-
-function addTrigger() {
-    deviceTrigger.value.friendly_name = device.value.friendly_name;
-    deviceTrigger.value.triggers.push(exposeTriggers[selectedExpose.value])
-
-    // reset state
-    conditions.value = []
-    exposeTriggers[selectedExpose.value] = null
-    selectedExpose.value = null
-}
 
 function getExposes() {
     return Object.keys(device.value.exposes)
@@ -68,23 +57,15 @@ function getExposes() {
 
 function create() {
 
-    var t = new DeviceTrigger()
-    t.friendly_name = device.value.friendly_name;
-    t.description = "TODO";// TODO;
-    // for (var key in Object.keys(exposeTriggers)) {
-    //     var exposeTrigger = exposeTriggers[key]
-    //     console.log("adding ", key, ":", exposeTrigger)
-    //     t.triggers.push(exposeTrigger)
-    // }
-    // console.log("new device Trigger:", t)
-
-    // emit("create", deviceTrigger.value)
+    var deviceTrigger = new DeviceTrigger()
+    deviceTrigger.friendly_name = device.value.friendly_name;
+    deviceTrigger.id = device.value.id
+    deviceTrigger.description = description.value
 
     for (const [key, item] of Object.entries(exposeTriggers)) {
-        console.log("key; ", key, "item ", item)
-        t.triggers.push(item)
+        deviceTrigger.triggers.push(item)
     }
-    console.log("new device Trigger:", t)
+    console.log("new device Trigger:", deviceTrigger)
     cancel();
 }
 
@@ -95,13 +76,7 @@ function cancel() {
 }
 
 function add(event) {
-    // TODO:
-    // to minizze the buttons eg get rid of add trigger button
-    // we can add the condition to conditions and add it to a dictionary with key of current expose
-    // only on final create we can construct the object
 
-    // TODO: !!!!!!11
-    // need to be able to exit from locked exposes options 
     conditions.value.push(event);
 
     var exposeTrigger = exposeTriggers[selectedExpose.value];
@@ -125,13 +100,13 @@ function remove(event) {
             </RouterLink>
         </div>
         <div class="col-3">
-            <input type="text" class="form-control" name="name" id="name" v-model="deviceTrigger.description"
-                placeholder="Name" onfocus="this.placeholder = ''" onblur="this.placeholder='Name'">
+            <input type="text" class="form-control" name="name" id="name" v-model="description" placeholder="Name"
+                onfocus="this.placeholder = ''" onblur="this.placeholder='Name'">
         </div>
 
         <div class="col-3">
             <select id="exposeSelector" style="text-align:center;" class="form-control" @change="exposeSelectionChanged"
-                v-model="selectedExpose" :disabled="selectedExpose != null">
+                v-model="selectedExpose">
                 <option :value="null">Select expose</option>
                 <option v-for="expose in device.exposes" :value="expose.name" :key="expose.name">
                     {{ expose.name }}
@@ -142,8 +117,8 @@ function remove(event) {
 
         <div>
             <div class="row w-50" v-if="selectedExpose != null">
-                <TriggerCondition :id="0" :exposes="getExposes()" :operator="Operators[0].value" :data="''"
-                    @add="add($event)">
+                <TriggerCondition :id="0" :exposes="getExposes()" :name="selectedExpose" :operator="Operators[0].value"
+                    :data="''" @add="add($event)">
                 </TriggerCondition>
             </div>
 
@@ -155,15 +130,9 @@ function remove(event) {
             </div>
             <div class="col-50">
                 <div class="btn-group">
-
-                    <!-- <button type="button" class="btn btn-default" :disabled="conditions.length == 0" @click="addTrigger">
-                        Add
-                    </button> -->
-
                     <button type="button" class="btn btn-default"
-                        :disabled="deviceTrigger.triggers.length == 0 && deviceTrigger.description.length == 0"
-                        @click="create">
-                        Done
+                        :disabled="exposeTriggers.length == 0 && description.length == 0" @click="create">
+                        Save
                     </button>
                 </div>
             </div>
