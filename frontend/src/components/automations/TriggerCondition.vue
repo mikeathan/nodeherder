c845bba5d67d9e5183fbe9077a600c391005761f
mikeathan <mikeathan@gmail.com>
2023-11-28 14:07:54 +0000

    clear data

--- frontend/src/components/automations/TriggerCondition.vue
+++ frontend/src/components/automations/TriggerCondition.vue
@@ -127,8 +127,7 @@ function exposeSelectionChanged(event) {
         return
     }
 
-    // var f = feature.value
-    // TODO: maybe reset operators select on change
+    data.value = "";
 }
 
 function operatorUpdated(event) {
