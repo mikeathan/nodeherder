ea5ede8850c13dac235768338d1b815693024345
mikeathan <mikeathan@gmail.com>
2023-12-11 20:57:54 +0000

    radio WIP

--- frontend/src/components/input/RadioGroup.vue
+++ frontend/src/components/input/RadioGroup.vue
@@ -16,34 +16,12 @@ const emit = defineEmits(['update:data'])
 </script>
 
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <form class="form-inline" action="#" method="post">
-
-
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                        <label class="form-check-label" for="inlineCheckbox1">1</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-                        <label class="form-check-label" for="inlineCheckbox2">2</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" disabled>
-                        <label class="form-check-label" for="inlineCheckbox3">3 (disabled)</label>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <div v-for="item in props.items">
+    pass in label / value eg disable/0, increase/1, decrease/2
+    <div v-for="item in props.items" class="col-xl-2">
         <div class="form-check form-check-inline">
-            <input class="form-check-input " type="radio" :name="`radio${item}`" :id="`radio${item}`" :value="item"
+            <input class="form-check-input " type="radio" name="radioSelection" id="radioSelection" :value="item"
                 v-model="props.value">
-            <label class="form-check-label" :for="`radio${item}`">
+            <label class="form-check-label" for="radioSelection">
                 {{ item }}
             </label>
         </div>
