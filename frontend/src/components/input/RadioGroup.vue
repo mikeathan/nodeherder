53c782a00e9ab8392a30cc2133320bd5427ce1c5
mikeathan <mikeathan@gmail.com>
2023-12-11 20:31:49 +0000

    aligh radio WIP

--- frontend/src/components/input/RadioGroup.vue
+++ frontend/src/components/input/RadioGroup.vue
@@ -13,11 +13,32 @@ const props = defineProps({
 
 const emit = defineEmits(['update:data'])
 
-
-
 </script>
 
 <template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <form class="form-inline" action="#" method="post">
+
+
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                        <label class="form-check-label" for="inlineCheckbox1">1</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                        <label class="form-check-label" for="inlineCheckbox2">2</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" disabled>
+                        <label class="form-check-label" for="inlineCheckbox3">3 (disabled)</label>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
     <div v-for="item in props.items">
         <div class="form-check form-check-inline">
             <input class="form-check-input " type="radio" :name="`radio${item}`" :id="`radio${item}`" :value="item"
