bc22014b495ac8e75c875430b3eb68ac29bf8596
mikeathan <mikeathan@gmail.com>
2023-12-12 07:25:10 +0000

    radio event wip

--- frontend/src/components/input/RadioGroup.vue
+++ frontend/src/components/input/RadioGroup.vue
@@ -14,16 +14,32 @@ const props = defineProps({
 
 const emit = defineEmits(['update:data'])
 
-function change(event) {
+const value = ref(null)
 
+watch(
+    () => props.value,
+    () => {
+        value.value = props.value
+    }, { immediate: true }
+)
+
+function selectionChanged(event) {
+
+    console.log(event.target.value)
+    if (event.target.value == null) {
+        return;
+    }
+    value.value = event.target.value;
+    emit("update:data", value.value);
 }
+
 </script>
 
 <template>
     <div v-for="item in props.items" class="col-xl-2">
         <div class="form-check form-check-inline">
-            <input class="form-check-input " type="radio" name="radioSelection" id="radioSelection" :value="item.value"
-                v-model="props.value" @change="change">
+            <input class="form-check-input " type="radio" name="radioSelection" id="radioSelection" :value="value"
+                v-model="props.value" @change="selectionChanged">
             <label class="form-check-label" for="radioSelection">
                 {{ item.name }}
             </label>
