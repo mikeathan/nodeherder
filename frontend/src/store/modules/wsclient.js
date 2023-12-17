926f6328d0895a29bd13597a1425b36b96bde549
mikeathan <mikeathan@gmail.com>
2023-12-17 19:52:14 +0000

    remove

--- frontend/src/store/modules/wsclient.js
+++ frontend/src/store/modules/wsclient.js
@@ -60,7 +60,6 @@ const actions = {
         case "devices":
           dispatch("devices/init", obj.payload, { root: true });
           break;
-          break;
         case "operationSuccess":
           //https://classic.yarnpkg.com/en/package/@kyvg/vue3-notification
           notify({
