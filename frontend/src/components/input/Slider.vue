d346101f381eb234eee4d687ce0cc9feacdf3da7
mikeathan <mikeathan@gmail.com>
2023-12-17 20:00:45 +0000

    note

--- frontend/src/components/input/Slider.vue
+++ frontend/src/components/input/Slider.vue
@@ -32,6 +32,7 @@ const min = ref()
 </script>
 
 <template>
+    https://github.com/vuejs/vue-eslint-parser/issues/49
     <div>
         <label v-if="props.placeholder != null" for="rangeSelector" class="form-label">{{ props.placeholder }}</label>
         <input type="range" class="form-range" id="rangeSelector" @change="valueChanged" v-model="value" :max="max"
