9d3be9a85561611a219c4d1e3c312ec16809fe85
mikeathan <mikeathan@gmail.com>
2023-12-18 20:36:57 +0000

    add toggle ui WIP

---
+++ frontend/src/components/input/Toggle.vue
@@ -0,0 +1,39 @@
+<script setup >
+import { ref, watch, watchEffect } from "vue";
+
+const emit = defineEmits(['update:value'])
+
+const props = defineProps({
+    placeholder:
+    {
+        type: String,
+        default: null
+    },
+    value: {
+        type: Boolean,
+        default: null
+    }
+});
+
+const value = ref(false)
+
+
+watch(
+    () => props.value,
+    () => {
+        value.value = props.value
+    }, { immediate: true }
+)
+function valueChanged(event) {
+    emit('update:value', event);
+}
+
+</script>
+
+<template>
+    <div>
+    </div>
+</template>
+
+
+
