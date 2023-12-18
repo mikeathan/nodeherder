117da626cff9b256f172f93a3190c68bbfc0568d
mike athan <mikeathan@gmail.com>
2023-12-18 15:55:10 +0000

    trigger automation after save

--- core/internal/controllers/hub-controller.go
+++ core/internal/controllers/hub-controller.go
@@ -98,12 +98,13 @@ func RegisterHubController(eventHub ws.EventHub, mqtt mqtt.MqttClient, repo devi
 		}
 
 		// trigger automation for changes to apply
-		// TODO: need to clear state or confim is clear
-		// device, err := h.registrar.LookupById(automation.Id)
-		// if err == nil {
-		// 	utils.LogInfof("Trigger automation %s[%s] after update", automation.FriendlyName, automation.Id)
-		// 	h.TriggerAutomation(device)
-		// }
+		if automation.Enabled {
+			device, err := h.registrar.LookupById(automation.Id)
+			if err == nil {
+				utils.LogInfof("Trigger automation %s[%s] after update", automation.FriendlyName, automation.Id)
+				h.TriggerAutomation(device)
+			}
+		}
 
 		return nil
 	})
