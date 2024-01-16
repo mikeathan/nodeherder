d3ed34a616d7a65b3a346e53e48245f784e5d69c
mikeathan <mikeathan@gmail.com>
2024-01-16 19:55:21 +0000

    configure jest for typescirpt

--- frontend/src/models/device/device.test.ts
+++ frontend/src/models/device/device.test.ts
@@ -1,61 +1,12 @@
-// {
-// "id": "0x00124b00146c31cd",
-// "friendly_name": "Motion sensor 1",
-// "description": "Motion sensor",
-// "connection_type": "mqtt",
-// "power_source": "Battery",
-// "exposes": {
-//   "occupancy": {
-//     "name": "occupancy",
-//     "description": "Indicates whether the device detected occupancy",
-//     "data": null,
-//     "type": "binary",
-//     "attributes": {
-//       "off": false,
-//       "on": true
-//     }
-//   },
-//   "temperature": {
-//     "name": "temperature",
-//     "description": "Measured temperature value",
-//     "unit": "°C",
-//     "data": null,
-//     "type": "numeric"
-//   }
-// },
-// "properties": {
-//   "availability": "offline"
-// }
-// }
-
+import "jest";
+import { describe, expect, test } from "@jest/globals";
 import { default as devicesObj } from "../../../../docs/devices.json";
-let device = devicesObj.payload[0];
-var json = JSON.stringify(device);
-const verifyResult = JSON.parse(json) as Device;
 
-type Device = {
-  id: string;
-  friendly_name: string;
-  description: string;
-  connection_type: string;
-  power_source: string;
-  exposes: { [key: string]: Expose };
-  properties: { [key: string]: any };
-};
+test("roundtrip serializing device", () => {
+  let device = devicesObj.payload[0];
+  var json = JSON.stringify(device);
+  const verifyResult = JSON.parse(json) as Device;
 
-type Expose = {
-  name: string;
-  description: string;
-  unit: string;
-  data: any | null;
-  type: string | null;
-  Attributes: { [key: string]: any } | null;
-};
-
-// import "jest";
-
-// describe("HelloWorld.vue", () => {
-//   test("renders props.msg when passed", () => {
-//     const msg = "new message";
-//   });
-// });
+  var json2 = JSON.stringify(verifyResult);
+  expect(json2).toBe(json);
+});
