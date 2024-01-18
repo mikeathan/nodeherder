58eabb127abd1f1b1b59ee1f83c725349b606606
mikeathan <mikeathan@gmail.com>
2024-01-18 19:35:16 +0000

    comment vue config settings. breaking build

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,25 +1,26 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   devServer: {
-    port: 4100
+    port: 4100,
   },
   transpileDependencies: true,
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: "source-map",
   },
-  configureWebpack: {
-    resolve: {
-      extensions: ['.ts', '.tsx', '.vue', '.js', '.json'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          options: { appendTsSuffixTo: [/\.vue$/] },
-          exclude: /node_modules/,
-        },
-      ],
-    },
-  },
+  // configureWebpack: {
+  //   resolve: {
+  //     extensions: ['.ts', '.tsx', '.vue', '.js', '.json'],
+  //   },
+  //   module: {
+  //     rules: [
+  //       {
+  //         test: /\.tsx?$/,
+  //         loader: 'ts-loader',
+  //         options: { appendTsSuffixTo: [/\.vue$/] },
+  //         exclude: /node_modules/,
+  //       },
+  //     ],
+  //   },
+  //},
 });
