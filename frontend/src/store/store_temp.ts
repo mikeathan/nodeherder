8aa3b7af695aae02c4753eab3736143568c0b8ba
mikeathan <mikeathan@gmail.com>
2024-01-18 20:39:51 +0000

    ts store wip

--- frontend/src/store/store_temp.ts
+++ frontend/src/store/store_temp.ts
@@ -1,6 +1,7 @@
 import { InjectionKey } from "vue";
-import { createStore, useStore as baseUseStore, Store } from "vuex";
+import { GetterTree, createStore, useStore as baseUseStore, Store } from "vuex";
 import { DeviceMap } from "./types/device";
+import { Device } from "../contracts/device";
 
 export interface State {
   devices: DeviceMap;
@@ -8,10 +9,32 @@ export interface State {
 // define injection key
 export const key: InjectionKey<Store<State>> = Symbol();
 
+export interface Getters extends GetterTree<State, State> {
+  find(state: State): Device;
+}
+
+const getters: Getters = {
+  find(state: State): Device {
+    return state.devices[0];
+  },
+};
+
 export const store_temp = createStore<State>({
   state: {
     devices: {},
   },
+  getters,
+  mutations: {
+    add(state: State, device: Device) {
+      state.devices[device.id] = device;
+    },
+  },
+  actions: {
+    incrementAsync({ commit }, device: Device) {
+      commit("add", device);
+    },
+  },
+  modules: {},
 });
 
 export function useStore_temp() {
