2ec2100b2932f5a459270898a8f59f82ae327d27
mikeathan <mikeathan@gmail.com>
2024-02-05 20:08:20 +0000

    wip

--- frontend/src/components/device/DevicePage.vue
+++ frontend/src/components/device/DevicePage.vue
@@ -9,7 +9,7 @@ import RenameDeviceDialog from "../dialogs/RenameDeviceDialog.vue";
 import { Device } from "@/types/device";
 import Tabs from '../controls/Tabs.vue'
 import Tab from '../controls/Tab.vue'
-import { DeviceInfo } from "../device/DeviceInfo.vue";
+import DeviceInfo from "../device/DeviceInfo.vue";
 const props = defineProps({
   id: String,
 });
@@ -24,12 +24,20 @@ const previousPage = computed(() => {
 });
 
 
-const deviceTabs = computed(() => {
+const devicePageTabs = computed(() => {
   const device = store.getters["devices/find"](props.id) as Device;
   if (device == undefined) {
-    return [DeviceInfo];
+    return [{
+      title: "Info",
+      enabled: true,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida purus vitae vulputate commodo."
+    }, {
+      title: "Something else",
+      enabled: false,
+      text: "Cras scelerisque, dolor vitae suscipit efficitur, risus orci sagittis velit, ac molestie nulla tortor id augue."
+    }
+    ];
   }
-
   return [
   ]
 });
@@ -39,61 +47,10 @@ const device = computed(() => {
   return store.getters["devices/find"](props.id);
 });
 
-function renameDevice(value: string) {
-  store.dispatch("devices/rename", { name: device.value.friendly_name, newName: value });
-}
-
-const displayProps = computed(() => {
-  const device = store.getters["devices/find"](props.id) as Device;
-  if (device == undefined) {
-    return [];
-  }
-  return [
-
-    {
-      key: "Id:",
-      value: device.id,
-    },
-    {
-      key: "Friendly name:",
-      value: device.friendly_name,
-    },
-    {
-      key: "Description:",
-      value: device.description,
-    },
-    {
-      key: "Availability:",
-      value: device.properties.availability,
-    },
-    {
-      key: "Last seen:",
-      type: LastSeen,
-      props: {
-        timestamp: device.properties.last_seen,
-      },
-    },
-    {
-      key: "Power source:",
-      type: PowerSource,
-      props: {
-        power_source: device.power_source,
-        value: device.properties.battery,
-      },
-    },
-    {
-      key: "Connection Type:",
-      type: ConnectionType,
-      props: {
-        type: device.connection_type,
-      },
-    },
-  ];
-});
 
 </script>
 <template>
-  <div>
+  <!-- <div>
     <Tabs>
       <Tab active="true" title="First Tab">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida purus vitae vulputate commodo.
@@ -109,41 +66,16 @@ const displayProps = computed(() => {
         hendrerit.
       </Tab>
     </Tabs>
-  </div>
-  <div v-if="device != null" class="panel">
-    <div className="panel-header d-flex flex-row">
-      <div class="align-self-center me-3">
-        <RouterLink :to="`${previousPage}`">
-          <i class="fa fa-arrow-left fa-xl" aria-hidden="true"></i>
-        </RouterLink>
-      </div>
-      <div class="h1 align-self-center">
-        {{ device.friendly_name }}
-      </div>
-    </div>
+  </div> -->
 
-    <div>
-      <dl className="row align-self-center" v-for="(prop, idx) in displayProps">
-        <dt className="col-12 col-md-5">{{ prop.key }}</dt>
-        <dd className="col-12 col-md-7 " v-if="prop.type == undefined">
-          <div title="last update" className="col text-truncate">
-            {{ prop.value }}
-          </div>
-        </dd>
-        <dd className="col-12 col-md-7" v-else>
-          <component :is="prop.type" v-bind="prop.props"></component>
-        </dd>
-      </dl>
-
-      <div class="btn-group btn-group-sm" role="group">
-        <button class="btn btn-default btn-number" title="Remove device" @click="showDialog = true">
-          <i class="far fa-edit"></i>
-        </button>
-      </div>
-    </div>
-
-    <RenameDeviceDialog :friendlyName="device.friendly_name" :show="showDialog" @update:name="renameDevice"
-      @close="e => showDialog = e">
-    </RenameDeviceDialog>
+  <div>
+    <Tabs>
+      <Tab active="true" title="info">
+        <component :is="DeviceInfo" v-model="props"></component>
+      </Tab>
+      <Tab title="Something else">
+        <component :is="DeviceInfo" v-model="props"></component>
+      </Tab>
+    </Tabs>
   </div>
 </template>
