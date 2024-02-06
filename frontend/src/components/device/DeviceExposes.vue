deb6b3dd3293e9298cd79bdf39849a691367803b
mikeathan <mikeathan@gmail.com>
2024-02-06 19:39:48 +0000

    update

---
+++ frontend/src/components/device/DeviceExposes.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { store } from "../../store/index";
+import { computed, ref } from "vue";
+import { useRouter } from "vue-router";
+import LastSeen from "../device/LastSeen.vue";
+import PowerSource from "../device/PowerSource.vue";
+import ConnectionType from "../device/ConnectionType.vue";
+import RenameDeviceDialog from "../dialogs/RenameDeviceDialog.vue";
+import { Device, Expose } from "@/types/device";
+
+const props = defineProps({
+    id: { type: String, required: true }
+});
+
+const device = computed(() => {
+    return store.getters["devices/find"](props.id) as Device;
+});
+
+const Exposes = computed(() => {
+
+    let list: Array<Expose> = []
+    Object.values(device.value.exposes).forEach(expose => {
+
+        list.push(expose)
+    });
+
+
+    return list;
+});
+</script>
+<template>
+    <div v-if="device != null" class="panel">
+        <div className="panel-header d-flex flex-row">
+        </div>
+        <div class="h1 align-self-center">
+            {{ device.friendly_name }}
+        </div>
+        if it has properties then its a feature <br>
+        if its numeric add slider <br>
+        if its binary add toggle <br>
+        if enum add radio group <br>
+        if no properties then just display values <br>
+        <div v-for="expose in Exposes">
+
+
+            {{ expose }}
+        </div>
+
+    </div>
+</template>
--- frontend/src/components/device/DevicePage.vue
+++ frontend/src/components/device/DevicePage.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import { store } from "../../store/index";
 import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
-import LastSeen from "../device/LastSeen.vue";
-import PowerSource from "../device/PowerSource.vue";
-import ConnectionType from "../device/ConnectionType.vue";
-import RenameDeviceDialog from "../dialogs/RenameDeviceDialog.vue";
-import { Device } from "@/types/device";
 import Tabs from '../controls/Tabs.vue'
 import Tab from '../controls/Tab.vue'
-import DeviceInfo from "../device/DeviceInfo.vue";
+import DeviceInfo from "./DeviceInfo.vue";
+import DeviceExposes from "./DeviceExposes.vue";
+
 const props = defineProps({
-  id: String,
+  id: { type: String, required: true }
 });
+
 
 const previousPage = computed(() => {
   const back = useRouter().options.history.state.back;
@@ -23,58 +20,16 @@ const previousPage = computed(() => {
   return useRouter().push("/");
 });
 
-
-const devicePageTabs = computed(() => {
-  const device = store.getters["devices/find"](props.id) as Device;
-  if (device == undefined) {
-    return [{
-      title: "Info",
-      enabled: true,
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida purus vitae vulputate commodo."
-    }, {
-      title: "Something else",
-      enabled: false,
-      text: "Cras scelerisque, dolor vitae suscipit efficitur, risus orci sagittis velit, ac molestie nulla tortor id augue."
-    }
-    ];
-  }
-  return [
-  ]
-});
-
-const showDialog = ref(false)
-const device = computed(() => {
-  return store.getters["devices/find"](props.id);
-});
-
-
 </script>
 <template>
-  <!-- <div>
-    <Tabs>
-      <Tab active="true" title="First Tab">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida purus vitae vulputate commodo.
-      </Tab>
-      <Tab title="Second Tab">
-        Cras scelerisque, dolor vitae suscipit efficitur, risus orci sagittis velit, ac molestie nulla tortor id augue.
-      </Tab>
-      <Tab title="Third Tab">
-        Morbi posuere, mauris eu vehicula tempor, nibh orci consectetur tortor, id eleifend dolor sapien ut augue.
-      </Tab>
-      <Tab title="Fourth Tab">
-        Aenean varius dui eget ante finibus, sit amet finibus nisi facilisis. Nunc pellentesque, risus et pretium
-        hendrerit.
-      </Tab>
-    </Tabs>
-  </div> -->
+  <div class="panel">
 
-  <div>
     <Tabs>
-      <Tab active="true" title="info">
-        <component :is="DeviceInfo" v-model="props"></component>
+      <Tab active="true" title="About">
+        <DeviceInfo :id="props.id"></DeviceInfo>
       </Tab>
-      <Tab title="Something else">
-        <component :is="DeviceInfo" v-model="props"></component>
+      <Tab title="Exposes">
+        <DeviceExposes :id="props.id"></DeviceExposes>
       </Tab>
     </Tabs>
   </div>
