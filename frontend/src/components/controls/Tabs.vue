cd5a795de6f31eca07862c82acf71d149b276355
mikeathan <mikeathan@gmail.com>
2024-02-12 20:10:36 +0000

    tab wip

--- frontend/src/components/controls/Tabs.vue
+++ frontend/src/components/controls/Tabs.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted, reactive } from 'vue';
 const props = defineProps(['customClass']);
 let tabContainer = ref(null);
+let tabHeaders = ref(null);
 let tabs = ref(null);
 let activeTabIndex = ref(0);
 
@@ -26,25 +27,27 @@ const changeTab = (index) => {
         x.classList.remove('active')
     }
     tabs.value[activeTabIndex].classList.add('active')
+    tabHeaders.value[activeTabIndex].classList.add('active')
 }
 
 </script>
 
 <template>
     <div id="tabs-container" :class="customClass" ref="tabContainer">
-
         <div id="tab-headers">
-            <ul class="nav nav-tabs ">
-                <li v-for=" (tab, index) in tabs" :key="index" class="nav-item" data-bs-toggle="tab" role="presentation">
+
+            <ul class="nav nav-tabs">
+                <li v-for=" (tab, index) in tabs" :key="index" class="nav-item" data-bs-toggle="tab" role="presentation"
+                    ref="tabHeaders">
                     <!-- @click.stop.prevent="setActive(tab)" -->
-                    <a href="#" class="nav-link" rrole="tab" data-toggle="tab"
+                    <a href="#" class="nav-link" role="tab" data-toggle="tab"
                         :class="activeTabIndex == index ? 'active' : ''" @click="changeTab(index)">{{ tab.title }}</a>
                 </li>
             </ul>
-            <ul id="active-tab" class="panel">
-                <slot></slot>
-            </ul>
         </div>
+        <ul id="active-tab" class="panel">
+            <slot></slot>
+        </ul>
 
     </div>
 
@@ -76,5 +79,23 @@ const changeTab = (index) => {
     </div> -->
 </template>
 
-<style></style>
+<style>
+.active a {
+    background-color: red !important;
+}
+
+this is what i want .tab-headers>.active a {
+    background-color: red !important;
+}
+
+
+.nav-tabs {
+    border: 0px solid transparent;
+}
+
+.nav-link {
+    border: 0px solid transparent;
+
+}
+</style>
 
