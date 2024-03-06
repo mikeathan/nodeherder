4c45c44a4f702f51725c94747f59bc4886bfa0b0
mike athan <mikeathan@gmail.com>
2024-03-06 16:55:27 +0000

    toggle action editor view

--- frontend/src/components/automations/actions/Action.vue
+++ frontend/src/components/automations/actions/Action.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, PropType, defineAsyncComponent, computed } from "vue";
+import { ref, watch, PropType, defineAsyncComponent, computed, onMounted } from "vue";
 import { getActionType, ActionType, AutomationActionTypes } from "@/contracts/automations"
 import { AutomationTriggerAction } from "@/types/automation";
 
@@ -28,7 +28,6 @@ const props = defineProps({
 const actionType = ref<ActionType>("TriggerAction");
 const isEditorView = ref<boolean>(false);
 
-
 watch(
     () => props.item,
     () => {
@@ -37,12 +36,23 @@ watch(
 )
 
 function saveAction(action: AutomationTriggerAction): void {
+    setEditorView(false);
     emit('save', action);
+}
+
+function setEditorView(enable: boolean): void {
+    isEditorView.value = enable;
 }
 
 function removeAction(action: AutomationTriggerAction): void {
     emit('delete', action);
 }
+
+onMounted(() => {
+    if (!actionView.value) {
+        isEditorView.value = true;
+    }
+});
 
 const actionView = computed(() => {
 
@@ -67,16 +77,19 @@ const actionView = computed(() => {
 </script>
 
 <template>
-    <div class="row" v-if="actionView != ''">
-        <div class=" col-sm-11">
-            {{ actionView }} double click to open editor
+    <div class="row" v-if="isEditorView == false">
+        <div class=" col-sm-11" @click="e => setEditorView(true)">
+            {{ actionView }}
         </div>
         <div class="col-sm-1">
             <span class="fa fa-trash-alt fa-sm" @click="e => removeAction(props.item)">
             </span>
         </div>
     </div>
-    <div v-else> if its isEditorView then show
+    <div v-else>
+        <div class="row">
+            <button type="button" class="btn-close" aria-label="Close" @click="e => setEditorView(false)"></button>
+        </div>
         <component :is="componentMap[actionType]" v-bind="{ action: props.item }" @delete="removeAction"
             @save="saveAction" />
     </div>
