642c74909d94495c1cb4f2e56711a0a78ceb89e4
mikeathan <mikeathan@gmail.com>
2024-03-30 20:43:46 +0000

    button panel

--- frontend/src/components/controls/ButtonPanel.vue
+++ frontend/src/components/controls/ButtonPanel.vue
@@ -1,43 +1,34 @@
 <script setup lang="ts">
-import { computed, ref, watchEffect, watch, PropType, reactive, h, VNode } from "vue";
-import Dropdown from "@/components/controls/Dropdown.vue";
-import Button from "@/components/controls/Button.vue";
-
+import { PropType, h, VNode } from "vue";
 import { ButtonPanelType, ButtonType, isDropdown, DropDownType } from "@/types/controls.type";
-import { PanelComponents } from "@/mixins/usePanelComponents";
-import { KeyyValuePair } from "@/types/types";
 
 function Panel() {
-
     let vNodes: VNode[] = [];
     props.buttons.forEach((item: ButtonPanelType) => {
-
-        if (isDropdown(item)) {
-            const dropDown = item as DropDownType;
-            const node = h("Dropdown", {
-                className: 'btn-light',
-                disabled: dropDown.disabled,
-                items: dropDown.items
-            }, dropDown.name);
-
-            vNodes.push(node);
-        } else {
-            const button = item as ButtonType;
-            const node = h("button", {
-                class: "btn btn-light",
-                disabled: button.disabled,
-                onClick(event: any) {
-                    button.click(event)
-                }
-            }, button.name);
-
-            vNodes.push(node);
-        }
-
+        const node = isDropdown(item) ? createDropdown(item as DropDownType) : createButton(item as ButtonType);
+        vNodes.push(node);
     });
     return vNodes;
 
 };
+
+function createDropdown(dropDown: DropDownType): VNode {
+    return h("Dropdown", {
+        className: 'btn-light',
+        disabled: dropDown.disabled,
+        items: dropDown.items
+    }, dropDown.name);
+}
+
+function createButton(button: ButtonType): VNode {
+    return h("button", {
+        class: "btn btn-light",
+        disabled: button.disabled,
+        onClick(event: any) {
+            button.click(event)
+        }
+    }, button.name);
+}
 
 const props = defineProps({
     buttons: {
@@ -53,9 +44,7 @@ const props = defineProps({
 <template>
     <div class="row pb-3">
         <form class="container">
-
             <Panel></Panel>
-
         </form>
     </div>
 </template>
