5bc7afdbeee9fd3c440bda6a003ee47a1dc3dc0f
mikeathan <mikeathan@gmail.com>
2024-05-06 20:08:58 +0100

    metrics wip

--- core/internal/metrics/metrics.go
+++ core/internal/metrics/metrics.go
@@ -1,29 +1,76 @@
 package repository
 
 import (
+	"encoding/json"
 	"errors"
 	"node-herder/models/devices"
+	"node-herder/utils"
 	"sort"
 	"sync"
+
+	"github.com/boltdb/bolt"
 )
+
+const filename = "metrics.db"
 
 type MetricsRepo struct {
 	store map[string]*devices.Device
 	mutex sync.RWMutex
+	db    *bolt.DB
 }
 
-func NewMetricsRepo() devices.Repository {
+func NewMetricsRepo() (devices.Repository, error) {
+
+	db, err := bolt.Open(filename, 0600, nil)
+	if err != nil {
+		utils.LogError(err)
+		return nil, err
+	}
+
 	return &MetricsRepo{
 		store: map[string]*devices.Device{},
 		mutex: sync.RWMutex{},
+		db:    db,
+	}, nil
+}
+
+func (s *MetricsRepo) Close() {
+	err := s.db.Close()
+	if err != nil {
+		utils.LogError(err)
 	}
 }
 
-func (s *MetricsRepo) Store(key string, device *devices.Device) {
+// todo
+// need new repository interface or match current ?
+// new struct for metrics
+// timestamp
+// key
+// data
+func (s *MetricsRepo) Store(key string, device *devices.Device) error {
 
 	defer s.mutex.Unlock()
 	s.mutex.Lock()
-	s.store[key] = device
+	s.db.Update(func(tx *bolt.Tx) error {
+		// Retrieve the users bucket.
+		// This should be created when the DB is first opened.
+		b := tx.Bucket([]byte("users"))
+
+		// Generate ID for the user.
+		// This returns an error only if the Tx is closed or not writeable.
+		// That can't happen in an Update() call so I ignore the error check.
+		id, _ := b.NextSequence()
+		u.ID = int(id)
+
+		// Marshal user data into bytes.
+		buf, err := json.Marshal(u)
+		if err != nil {
+			return err
+		}
+
+		// Persist bytes to users bucket.
+		return b.Put(utils.Itob(u.ID), buf)
+	})
 }
 
 func (s *MetricsRepo) FindDevice(id string) (*devices.Device, error) {
