d5e20cdd7102f4165d96762599238fa86b932ada
mikeathan <mikeathan@gmail.com>
2024-05-10 16:52:42 +0100

    metrics wip

--- core/internal/metrics/metrics.go
+++ core/internal/metrics/metrics.go
@@ -1,12 +1,13 @@
 package repository
 
 import (
+	"bytes"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"node-herder/models/devices"
 	"node-herder/utils"
-	"sort"
 	"sync"
+	"time"
 
 	"github.com/boltdb/bolt"
 )
@@ -19,7 +20,7 @@ type MetricsRepo struct {
 	db    *bolt.DB
 }
 
-func NewMetricsRepo() (devices.Repository, error) {
+func NewMetricsRepo() (devices.MetricsRepository, error) {
 
 	db, err := bolt.Open(filename, 0600, nil)
 	if err != nil {
@@ -52,71 +53,52 @@ func (s *MetricsRepo) Store(key string, device *devices.Device) error {
 	defer s.mutex.Unlock()
 	s.mutex.Lock()
 	s.db.Update(func(tx *bolt.Tx) error {
-		// Retrieve the users bucket.
-		// This should be created when the DB is first opened.
-		b := tx.Bucket([]byte("users"))
 
-		// Generate ID for the user.
-		// This returns an error only if the Tx is closed or not writeable.
-		// That can't happen in an Update() call so I ignore the error check.
-		id, _ := b.NextSequence()
-		u.ID = int(id)
-
-		// Marshal user data into bytes.
-		buf, err := json.Marshal(u)
+		bucket, err := tx.CreateBucketIfNotExists([]byte("metrics"))
 		if err != nil {
 			return err
 		}
 
-		// Persist bytes to users bucket.
-		return b.Put(utils.Itob(u.ID), buf)
+		// This returns an error only if the Tx is closed or not writeable.
+		// That can't happen in an Update() call so I ignore the error check.
+		id, _ := bucket.NextSequence()
+
+		// Marshal user data into bytes.
+		buf, err := json.Marshal(device)
+		if err != nil {
+			return err
+		}
+
+	 store using device.Id  and timestamp
+		// WIP - refactor
+		lastSeenStr, _ := device.Properties["last_seen"].(string)
+		lastSeen, err := time.Parse(time.RFC3339, lastSeenStr)
+		if err != nil {
+			return err
+		}
+		//
+		return bucket.Put([]byte(lastSeen.Format(time.RFC3339)), buf)
+	})
+
+	return nil
+}
+
+func (s *MetricsRepo) ViewRange(device *devices.Device, from time.Time, to time.Time) error {
+
+	device.Id 
+	return s.db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte("metrics")).Cursor()
+		min := []byte(from.Format(time.RFC3339))
+		max := []byte(to.AddDate(0, 0, 0).Format(time.RFC3339))
+
+		for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) <= 0; k, v = c.Next() {
+			fmt.Println(string(k), string(v))
+		}
+
+		return nil
 	})
 }
 
-func (s *MetricsRepo) FindDevice(id string) (*devices.Device, error) {
-
-	defer s.mutex.RUnlock()
-
-	s.mutex.RLock()
-	if val, ok := s.store[id]; ok {
-		return val, nil
-	}
-
-	return nil, errors.New("device not found")
-}
-
-func (s *MetricsRepo) FindDevices(ids []string) []*devices.Device {
-	ds := []*devices.Device{}
-
-	defer s.mutex.RUnlock()
-
-	s.mutex.RLock()
-	for _, id := range ids {
-		if val, ok := s.store[id]; ok {
-			ds = append(ds, val)
-		}
-	}
-
-	return ds
-}
-func (s *MetricsRepo) AllDevices() []*devices.Device {
-
-	// sort before returning values
-	s.mutex.RLock()
-	defer s.mutex.RUnlock()
-
-	keys := make([]string, 0, len(s.store))
-	for k := range s.store {
-		keys = append(keys, k)
-	}
-
-	sort.Strings(keys)
-
-	devices := make([]*devices.Device, 0, len(s.store))
-	for _, key := range keys {
-		device := s.store[key]
-		devices = append(devices, device)
-	}
-
-	return devices
+func (s *MetricsRepo) ViewDevice(from time.Time, to time.Time) error {
+	return nil
 }
--- core/models/devices/repository.go
+++ core/models/devices/repository.go
@@ -1,8 +1,14 @@
 package devices
 
+// this is going to be DeviceRepository
 type Repository interface {
 	AllDevices() []*Device
 	Store(key string, device *Device)
 	FindDevice(key string) (*Device, error)
 	FindDevices(ids []string) []*Device
+}
+
+type MetricsRepository interface {
+	Store(key string, device *Device) error
+	Close()
 }
