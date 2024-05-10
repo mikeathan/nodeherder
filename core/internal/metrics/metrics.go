4ba62bd470d4e65de45b9ea4b8ecebedbceb33fc
mikeathan <mikeathan@gmail.com>
2024-05-10 17:42:04 +0100

    wip

--- core/internal/metrics/metrics.go
+++ core/internal/metrics/metrics.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"node-herder/models/devices"
@@ -61,23 +62,14 @@ func (s *MetricsRepo) Store(key string, device *devices.Device) error {
 
 		// This returns an error only if the Tx is closed or not writeable.
 		// That can't happen in an Update() call so I ignore the error check.
-		id, _ := bucket.NextSequence()
+		bucket.NextSequence()
 
-		// Marshal user data into bytes.
 		buf, err := json.Marshal(device)
 		if err != nil {
 			return err
 		}
 
-	 store using device.Id  and timestamp
-		// WIP - refactor
-		lastSeenStr, _ := device.Properties["last_seen"].(string)
-		lastSeen, err := time.Parse(time.RFC3339, lastSeenStr)
-		if err != nil {
-			return err
-		}
-		//
-		return bucket.Put([]byte(lastSeen.Format(time.RFC3339)), buf)
+		return bucket.Put(createKeyFromDevice(device), buf)
 	})
 
 	return nil
@@ -85,20 +77,91 @@ func (s *MetricsRepo) Store(key string, device *devices.Device) error {
 
 func (s *MetricsRepo) ViewRange(device *devices.Device, from time.Time, to time.Time) error {
 
-	device.Id 
 	return s.db.View(func(tx *bolt.Tx) error {
-		c := tx.Bucket([]byte("metrics")).Cursor()
-		min := []byte(from.Format(time.RFC3339))
-		max := []byte(to.AddDate(0, 0, 0).Format(time.RFC3339))
+		bucket := tx.Bucket([]byte("metrics")).Cursor()
+		if bucket == nil {
+			return bolt.ErrBucketNotFound
+		}
 
-		for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) <= 0; k, v = c.Next() {
+		fromKey := createKeyWithTimestamp(device, from)
+		tokey := createKeyWithTimestamp(device, to)
+
+		for k, v := bucket.Seek(fromKey); k != nil && bytes.Compare(k, tokey) <= 0; k, v = bucket.Next() {
 			fmt.Println(string(k), string(v))
+
+			// var point SensorData
+			// 	if err := json.Unmarshal(v, &point); err != nil {
+			// 		return err
+			// 	}
+			// 	data = append(data, point)
 		}
 
 		return nil
 	})
 }
 
-func (s *MetricsRepo) ViewDevice(from time.Time, to time.Time) error {
-	return nil
+func createKeyWithTimestamp(device *devices.Device, timestamp time.Time) []byte {
+
+	buffer := bytes.NewBuffer(nil)
+	binary.Write(buffer, binary.BigEndian, []byte(device.Id))
+	binary.Write(buffer, binary.BigEndian, timestamp.UnixMilli())
+
+	// Add a separator byte
+	buffer.WriteByte(0)
+
+	return buffer.Bytes()
 }
+
+func createKeyFromDevice(device *devices.Device) []byte {
+
+	lastSeenStr, _ := device.Properties["last_seen"].(string)
+	lastSeen, err := time.Parse(time.RFC3339, lastSeenStr)
+	if err != nil {
+		lastSeen = time.Now()
+	}
+
+	return createKeyWithTimestamp(device, lastSeen)
+}
+
+// // Paginate entries
+// pageSize := 10
+// pageNumber := 1
+
+// err = db.View(func(tx *bolt.Tx) error {
+// 	bucket := tx.Bucket([]byte("entries"))
+// 	if bucket == nil {
+// 		return fmt.Errorf("Bucket not found")
+// 	}
+
+// 	// Start pagination from the specified page number
+// 	cursor := bucket.Cursor()
+
+// 	// Calculate the offset to start pagination from
+// 	offset := (pageNumber - 1) * pageSize
+
+// 	// Iterate over keys
+// 	count := 0
+// 	for k, v := cursor.First(); k != nil; k, v = cursor.Next() {
+// 		// Skip until the offset is reached
+// 		if count < offset {
+// 			count++
+// 			continue
+// 		}
+
+// 		// Print key and value
+// 		fmt.Printf("Key: %s, Value: %s\n", k, v)
+
+// 		// Break the loop when pageSize entries are printed
+// 		if count >= offset+pageSize {
+// 			break
+// 		}
+
+// 		count++
+// 	}
+
+// 	return nil
+// })
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// }
