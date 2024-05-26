704baa58ae4ebb8a6528c2d5ed686fe2d756cedb
mikeathan <mikeathan@gmail.com>
2024-05-26 19:52:43 +0100

    reafactorying repos

--- core/models/settings/repository.go
+++ core/models/settings/repository.go
@@ -1,5 +1,25 @@
 package settings
 
+type AppConfig struct {
+	Devices map[string]*DeviceConfig `json:"devices"`
+}
+
+func NewAppConfig() *AppConfig {
+	return &AppConfig{
+		Devices: map[string]*DeviceConfig{},
+	}
+}
+
+func (s *AppConfig) Add(cfg *DeviceConfig) {
+	s.Devices[cfg.Id] = cfg
+}
+
+type DeviceConfig struct {
+	Id       string `json:"id"`
+	Disabled bool   `json:"disabled"`
+	History  bool   `json:"history"`
+}
+
 type Repository interface {
 	Save(config *AppConfig) error
 	Load() (*AppConfig, error)
