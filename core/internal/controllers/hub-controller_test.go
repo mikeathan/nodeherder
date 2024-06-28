56e2d13d139bee6557b391193b3966f302dfc41c
mikeathan <mikeathan@gmail.com>
2024-06-28 20:04:48 +0100

    todo

--- core/internal/controllers/hub-controller_test.go
+++ core/internal/controllers/hub-controller_test.go
@@ -115,7 +115,57 @@ func TestProcessorTriggersStepActionDialAutomations(t *testing.T) {
 	wg.Wait()
 }
 
-func TestProcessorTriggersAutomationsStoresMetrics(t *testing.T) {
+func TestProcessorTriggersAutomationsStoresMetricsForNewDevice(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	mqtt := &mocks.MockMqttClient{}
+	ws := &mocks.NopWsServer{}
+
+	// setup device
+	device1Expose1 := utils_test.CreateEnumEntity("action", utils_test.CreateDialActionEnums())
+	device1Expose2 := utils_test.CreateNumericEntity("action_time", 0)
+	dialDevice := utils_test.CreateDeviceWithExposes("x01111111", "Dial button", []*devices.Entity{device1Expose1, device1Expose2})
+
+	device2Expose1 := utils_test.CreateEntity("brightness", "numeric", nil)
+	device2Expose2 := utils_test.CreateEnumEntity("color_temp", utils_test.CreateColorTempPresets())
+	lightDevice := utils_test.CreateDeviceWithExposes("x02222222", "Attic light", []*devices.Entity{device2Expose1, device2Expose2})
+
+	// setup bridgeInfo List
+	devices := []*devices.Device{dialDevice}
+	deviceBridgeList := utils_test.CreateBridgeInfoList(devices) // NEED TO FIX, currently i make all devices features which is not right!!!!
+
+	// register hub
+	store, cleanup, err := utils_test.CreateFileStore()
+	if err != nil {
+		t.Fatalf("CreateFileStore failed. err %v ", err)
+	}
+	defer cleanup()
+
+	// enable metrics for dial device
+	cfg := settings.NewDeviceConfig("x02222222")
+	cfg.MetricsEnabled = true
+	cfg.RateLimit = 10 // 10 ms
+	store.SaveDeviceConfig(cfg)
+
+	controllers.RegisterHubController(ws, store, mqtt, context.Background())
+
+	mqtt.Publish("bridge/devices", deviceBridgeList)
+	time.Sleep(500 * time.Millisecond) // give it time to configure bridgeInfo
+	//  SETUP END
+
+
+	// update dial button device - shouldne be stored as metrics 
+	payload := map[string]any{"action": "button_2_hold"}
+	mqtt.Publish(dialDevice.FriendlyName, payload)
+
+
+	// publish new device
+	payload = map[string]any{"brightness": 10.0, "color_temp": 100}
+	mqtt.Publish(lightDevice.FriendlyName, payload)
+
+	TEST New device is stored corectly
+
+}
+func TestProcessorTriggersAutomationsStoresMetricsForExistingDevice(t *testing.T) {
 
 	wg := &sync.WaitGroup{}
 	mqtt := &mocks.MockMqttClient{}
