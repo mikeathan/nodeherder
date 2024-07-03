4bc8047ee5fdc177c487cf67ceef3bb7d867124b
mikeathan <mikeathan@gmail.com>
2024-07-03 16:45:36 +0100

    device settings component

--- frontend/src/components/device/DeviceSettings.vue
+++ frontend/src/components/device/DeviceSettings.vue
@@ -1,74 +1,88 @@
 <script setup lang="ts">
 import { store } from "../../store/index";
-import { computed, ref } from "vue";
-import { Device, Expose } from "@/types/device";
-import { ExposeTypes } from "@/types/device.type";
-import { getExposeAttribute, getExposeProperty } from "../../contracts/device";
-
-import Slider from "../input/Slider.vue";
+import { Ref, computed, onMounted, ref } from "vue";
 import Toggle from "../input/Toggle.vue";
-import RadioGroup from "../input/RadioGroup.vue";
-import { getSensorUnit, getSensorValue } from "@/modules/formatters/sensor-formatter";
 import { DeviceSettings } from "@/types/settings";
 import { createDeviceSettings } from "@/contracts/settings";
 import InputBox from "../input/InputBox.vue";
+import { KeyyValuePair } from "@/types/types";
 
 const props = defineProps({
-    id: { type: String, required: true }
+  id: { type: String, required: true },
+});
+const cachedDeviceSettings = ref<KeyyValuePair<any>>({} as KeyyValuePair<any>);
+const isDirty = computed(() => {
+  const res =
+    JSON.stringify(cachedDeviceSettings.value) !==
+    JSON.stringify(deviceSettings.value);
+
+  console.log("isDirty", res);
+  return res;
 });
 
 const deviceSettings = computed(() => {
-    if (!store.getters["appconfig/initialized"]() as Boolean) {
-        store.dispatch('ws/emit', { event: "loadAppConfig" });
-    }
+  if (!store.getters["appconfig/initialized"]() as Boolean) {
+    store.dispatch("ws/emit", { event: "loadAppConfig" });
+  }
 
-    const settings = store.getters["appconfig/findDeviceSetting"](props.id) as DeviceSettings;
-    if (!settings) {
-        const newDeviceSettings = createDeviceSettings(props.id)
-        store.dispatch('appconfig/saveDeviceSettings', newDeviceSettings)
-        return newDeviceSettings
-    }
+  const settings = store.getters["appconfig/findDeviceSetting"](props.id);
 
-    return settings;
+  if (!settings) {
+    const newDeviceSettings = JSON.parse(
+      JSON.stringify(createDeviceSettings(props.id))
+    );
+    store.dispatch("appconfig/saveDeviceSettings", newDeviceSettings);
+    return newDeviceSettings;
+  }
+  return JSON.parse(JSON.stringify(settings));
 });
 
-
-const settingsElements = computed(() => {
-
-    return Object.entries(deviceSettings.value);
+onMounted(() => {
+  cachedDeviceSettings.value = JSON.parse(JSON.stringify(deviceSettings.value));
+  console.log("mounted ", cachedDeviceSettings.value);
 });
 
 function updateValue(propName: any, propValue: any) {
-    settingsElements.value[propName] = propValue;
-
-    console.log("uodate value ", propName, settingsElements.value[propName])
-
-    //store.dispatch('appconfig/saveDeviceSettings', deviceSettings.value)
+  cachedDeviceSettings.value[propName] = propValue;
+  console.log(
+    "updating ",
+    propName,
+    deviceSettings.value[propName],
+    "cached",
+    cachedDeviceSettings.value[propName]
+  );
 }
 
 function save() {
-    console.log("save ", deviceSettings.value)
-    store.dispatch('appconfig/saveDeviceSettings', deviceSettings.value)
+  console.log("save ", cachedDeviceSettings.value as DeviceSettings);
+  store.dispatch(
+    "appconfig/saveDeviceSettings",
+    cachedDeviceSettings.value as DeviceSettings
+  );
 }
-
-
 </script>
 <template>
-    <form>
+  {{ deviceSettings }}
 
-        <div v-for="([key, value]) in settingsElements">
-            <label>{{ key }}</label>
+  <form @submit.prevent="save()">
+    <div v-for="(value, key) in deviceSettings" :key="key">
+      <label>{{ key }}</label>
 
-            <div v-if="typeof value === 'boolean'">
-                <Toggle :minimal="false" :value="value" valueOn="enable" valueoff="disable"
-                    @update="(v) => updateValue(key, v)">
-                </Toggle>
-            </div>
-            <div v-else>
-                <InputBox @updated="(v) => updateValue(key, v)" :value="value">
-                </InputBox>
-            </div>
-        </div>
-        <button :onclick="save()">Save</button>
-    </form>
+      <div v-if="typeof value === 'boolean'">
+        <Toggle
+          :minimal="false"
+          :value="value"
+          :valueOn="true"
+          :valueoff="false"
+          @update="(v) => updateValue(key, v)"
+        >
+        </Toggle>
+      </div>
+      <div v-else>
+        <InputBox @updated="(v) => updateValue(key, v)" :value="value">
+        </InputBox>
+      </div>
+    </div>
+    <button type="button" @click="save()" :disabled="!isDirty">Save</button>
+  </form>
 </template>
