875981d73352e40588b99ca4ebf4c527a91da9bd
mikeathan <mikeathan@gmail.com>
2024-07-04 16:36:34 +0100

    format

--- frontend/src/components/device/DeviceSettings.vue
+++ frontend/src/components/device/DeviceSettings.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import { store } from "../../store/index";
-import { computed, onMounted, ref } from "vue";
-import Toggle from "../input/Toggle.vue";
-import { DeviceSettings } from "@/types/settings";
-import { createDeviceSettings } from "@/contracts/settings";
-import InputBox from "../input/InputBox.vue";
-import { KeyyValuePair } from "@/types/types";
+import { store } from '../../store/index';
+import { computed, onMounted, ref } from 'vue';
+import Toggle from '../input/Toggle.vue';
+import { DeviceSettings } from '@/types/settings';
+import { createDeviceSettings } from '@/contracts/settings';
+import InputBox from '../input/InputBox.vue';
+import { KeyyValuePair } from '@/types/types';
 
 const props = defineProps({
   id: { type: String, required: true },
 });
-const cachedDeviceSettings = ref<KeyyValuePair<any>>({} as KeyyValuePair<any>);
+const cachedDeviceSettings = ref<KeyyValuePair<any>>(
+  {} as KeyyValuePair<any>,
+);
 const isDirty = computed(() => {
   return (
     JSON.stringify(cachedDeviceSettings.value) !==
@@ -19,24 +21,33 @@ const isDirty = computed(() => {
 });
 
 const deviceSettings = computed(() => {
-  if (!store.getters["appconfig/initialized"]() as Boolean) {
-    store.dispatch("ws/emit", { event: "loadAppConfig" });
+  if (
+    !store.getters['appconfig/initialized']() as Boolean
+  ) {
+    store.dispatch('ws/emit', { event: 'loadAppConfig' });
   }
 
-  const settings = store.getters["appconfig/findDeviceSetting"](props.id);
+  const settings = store.getters[
+    'appconfig/findDeviceSetting'
+  ](props.id);
 
   if (!settings) {
     const newDeviceSettings = JSON.parse(
-      JSON.stringify(createDeviceSettings(props.id))
+      JSON.stringify(createDeviceSettings(props.id)),
     );
-    store.dispatch("appconfig/saveDeviceSettings", newDeviceSettings);
+    store.dispatch(
+      'appconfig/saveDeviceSettings',
+      newDeviceSettings,
+    );
     return newDeviceSettings;
   }
   return JSON.parse(JSON.stringify(settings));
 });
 
 onMounted(() => {
-  cachedDeviceSettings.value = JSON.parse(JSON.stringify(deviceSettings.value));
+  cachedDeviceSettings.value = JSON.parse(
+    JSON.stringify(deviceSettings.value),
+  );
 });
 
 function updateValue(propName: any, propValue: any) {
@@ -45,23 +56,36 @@ function updateValue(propName: any, propValue: any) {
 
 function save() {
   store.dispatch(
-    "appconfig/saveDeviceSettings",
-    cachedDeviceSettings.value as DeviceSettings
+    'appconfig/saveDeviceSettings',
+    cachedDeviceSettings.value as DeviceSettings,
   );
 }
 </script>
 <template>
-  <div class="row border-bottom py-1 w-100 align-items-center" v-for="(value, key) in deviceSettings" :key="key">
+  <div
+    class="row border-bottom py-1 w-100 align-items-center"
+    v-for="(value, key) in deviceSettings"
+    :key="key">
     <dl class="col-12 col-md-3">
-      <dt><strong> {{ key }}</strong></dt>
+      <dt>
+        <strong> {{ key }}</strong>
+      </dt>
     </dl>
     <div class="col-md-4">
       <div v-if="typeof value === 'boolean'">
-        <Toggle :minimal="false" :value="value" :valueOn="true" :valueoff="false" @update="(v) => updateValue(key, v)">
+        <Toggle
+          :minimal="false"
+          :value="value"
+          :valueOn="true"
+          :valueoff="false"
+          @update="(v) => updateValue(key, v)">
         </Toggle>
       </div>
       <div v-else>
-        <InputBox @updated="(v) => updateValue(key, v)" :value="value" :disabled="typeof value !== 'number'"
+        <InputBox
+          @updated="(v) => updateValue(key, v)"
+          :value="value"
+          :disabled="typeof value !== 'number'"
           :is-numeric="typeof value === 'number'">
         </InputBox>
       </div>
@@ -69,7 +93,12 @@ function save() {
   </div>
   <br />
   <div class="pb-3">
-    <button type="button" class="btn btn-light" aria-label="Save" @click="save()" :disabled="!isDirty">
+    <button
+      type="button"
+      class="btn btn-light"
+      aria-label="Save"
+      @click="save()"
+      :disabled="!isDirty">
       Save
     </button>
   </div>
