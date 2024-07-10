07aa0148c257cb98e966886ee9fdb94259e4799d
mikeathan <mikeathan@gmail.com>
2024-07-10 13:15:23 +0100

    add chart lib

---
+++ frontend/src/components/device/DeviceChart.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { ref, onBeforeMount } from 'vue';
+import type { Ref } from 'vue';
+import { Bar, Line } from 'vue-chartjs';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Chart,
+  ChartData,
+  type ChartOptions,
+  ChartDataset,
+} from 'chart.js';
+
+const data: ChartData = {
+  labels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [
+        40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11,
+      ],
+    },
+  ],
+};
+const props = defineProps({
+  chartData: {
+    type: Object,
+    default: null,
+  },
+  options: {
+    type: Object,
+    default: null,
+  },
+});
+
+const chartOptions = ref({
+  responsive: true,
+  maintainAspectRatio: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+});
+
+const chartData = ref({
+  labels: ['January', 'February', 'March'],
+  datasets: [{ data: [50, 30, 35] }],
+}) as Ref<ChartData>;
+
+onBeforeMount(() => {
+  ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    PointElement,
+    LineElement,
+    LinearScale,
+  );
+});
+</script>
+
+<template>
+  <Line :options="chartOptions" :data="data" />
+</template>
--- frontend/src/components/device/DeviceMetrics.vue
+++ frontend/src/components/device/DeviceMetrics.vue
@@ -2,80 +2,82 @@
 import { store } from '../../store/index';
 import { computed, onMounted, ref } from 'vue';
 import {
-    DeviceMetricsRequest,
-    DeviceMetrics,
+  DeviceMetricsRequest,
+  DeviceMetrics,
 } from '@/types/metrics';
 import Selection from '../input/Selection.vue';
+import DeviceChart from '../device/DeviceChart.vue';
+
 import { KeyyValuePair } from '@/types/types';
 import { toUnix } from '@/utils/date.utils';
 
 const props = defineProps({
-    id: { type: String, required: true },
+  id: { type: String, required: true },
 });
 
 const historySelection: string[] = [
-    'last hour',
-    'last day',
-    'last week',
-    'last month',
+  'last hour',
+  'last day',
+  'last week',
+  'last month',
 ];
 const timeOffsets: KeyyValuePair<number> = {
-    'last hour': 1,
-    'last day': 24,
-    'last week': 168,
-    'last month': 720,
+  'last hour': 1,
+  'last day': 24,
+  'last week': 168,
+  'last month': 720,
 };
 const fromDate = ref<Date | null>(null);
 const toDate = ref<Date | null>(null);
 
 function dateSelected(value: any) {
-    const dateOffset = timeOffsets[value];
+  const dateOffset = timeOffsets[value];
 
-    toDate.value = new Date();
-    fromDate.value = new Date();
-    fromDate.value.setHours(
-        toDate.value.getHours() - dateOffset,
-    );
+  toDate.value = new Date();
+  fromDate.value = new Date();
+  fromDate.value.setHours(
+    toDate.value.getHours() - dateOffset,
+  );
 
-    var request: DeviceMetricsRequest = {
-        id: props.id,
-        from: toUnix(fromDate.value ?? new Date()), // temp
-        to: toUnix(toDate.value ?? new Date()), // temp
-    };
+  var request: DeviceMetricsRequest = {
+    id: props.id,
+    from: toUnix(fromDate.value ?? new Date()), // temp
+    to: toUnix(toDate.value ?? new Date()), // temp
+  };
 
-    store.dispatch('metrics/query', request);
+  store.dispatch('metrics/query', request);
 }
 
 const metricsRequest = ref<
-    KeyyValuePair<DeviceMetricsRequest>
+  KeyyValuePair<DeviceMetricsRequest>
 >({} as KeyyValuePair<DeviceMetricsRequest>);
 
-
 const deviceMetrics = computed(() => {
-    const results = store.getters['metrics/view'](
-        props.id,
-    );
-    // if (results == null) {
-    //     var request: DeviceMetricsRequest = {
-    //         id: props.id,
-    //         from: toUnix(fromDate.value ?? new Date()), // temp
-    //         to: toUnix(toDate.value ?? new Date()), // temp
-    //     };
+  const results = store.getters['metrics/view'](props.id);
+  // if (results == null) {
+  //     var request: DeviceMetricsRequest = {
+  //         id: props.id,
+  //         from: toUnix(fromDate.value ?? new Date()), // temp
+  //         to: toUnix(toDate.value ?? new Date()), // temp
+  //     };
 
-    //     store.dispatch('metrics/query', request);
-    // }
+  //     store.dispatch('metrics/query', request);
+  // }
 
-    return results;
+  return results;
 });
-
-
 </script>
 <style scoped></style>
 
 <template>
-    <div class="col-sm-3">
-        <Selection label="Select time offset:" @updated="dateSelected" :items="historySelection">
-        </Selection>
-    </div>
-    {{ deviceMetrics }}
+  <div class="col-sm-3">
+    <Selection
+      label="Select time offset:"
+      @updated="dateSelected"
+      :items="historySelection">
+    </Selection>
+  </div>
+  {{ deviceMetrics }}
+
+  <DeviceChart></DeviceChart>
 </template>
