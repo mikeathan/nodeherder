e1dc8dbb70c9b161f1670aa71d8d9ee02c06c84f
mikeathan <mikeathan@gmail.com>
2024-08-20 19:59:40 +0100

    configure ws

--- core/internal/ws/ws.go
+++ core/internal/ws/ws.go
@@ -47,7 +47,7 @@ const (
 	AppConfig = "appConfig"
 
 	// Maximum message size allowed from peer.
-	maxMessageSize = 5 * 1024 * 1024
+	maxMessageSize = 1 * 1024 * 1024
 
 	// Time allowed to write a message to the peer.
 	writeWait = 10 * time.Second
@@ -119,45 +119,20 @@ type wsServer struct {
 	onSaveDeviceConfig        func(interface{}) error
 }
 
-// func New() *Melody {
-// 	upgrader := &websocket.Upgrader{
-// 		ReadBufferSize:  1024,
-// 		WriteBufferSize: 1024,
-// 		CheckOrigin:     func(r *http.Request) bool { return true },
-// 	}
-
-// 	hub := newHub()
-
-// 	go hub.run()
-
-// 	return &Melody{
-// 		Config:                   newConfig(),
-// 		Upgrader:                 upgrader,
-// 		messageHandler:           func(*Session, []byte) {},
-// 		messageHandlerBinary:     func(*Session, []byte) {},
-// 		messageSentHandler:       func(*Session, []byte) {},
-// 		messageSentHandlerBinary: func(*Session, []byte) {},
-// 		errorHandler:             func(*Session, error) {},
-// 		closeHandler:             nil,
-// 		connectHandler:           func(*Session) {},
-// 		disconnectHandler:        func(*Session) {},
-// 		pongHandler:              func(*Session) {},
-// 		hub:                      hub,
-// 	}
-// }
-
 func NewWsHub() EventHub {
 
 	server := melody.New()
 	server.Upgrader.ReadBufferSize = maxMessageSize
+	server.Upgrader.WriteBufferSize = maxMessageSize
 	server.Config.PingPeriod = pingPeriod
 	server.Config.PongWait = pongWait
 	server.Config.WriteWait = writeWait
 	server.Config.MaxMessageSize = maxMessageSize
+	server.Config.MessageBufferSize = maxMessageSize
 	//		ConcurrentMessageHandling bool          // Handle messages from sessions concurrently.
 
 	wsHub := &wsServer{
-		server:                    melody.New(),
+		server:                    server,
 		clients:                   map[int64]bool{},
 		onSaveAutomation:          func(payload interface{}) error { return nil },
 		onLoadMetrics:             func(interface{}) (interface{}, error) { return nil, nil },
