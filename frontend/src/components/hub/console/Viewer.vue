6517fab879236aee8a7d976da6af081d0103b67c
mikeathan <mikeathan@gmail.com>
2024-10-08 19:47:19 +0100

    todo

--- frontend/src/components/hub/console/Viewer.vue
+++ frontend/src/components/hub/console/Viewer.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { key, store } from "../../../store/index";
-import HistorySettings from './HistorySettings.vue';
-import { HistorySettingsType } from "@/types/settings";
+import Toggle from "../../input/Toggle.vue"
+
 
 function enableLogging(enabled: boolean) {
     store.dispatch('ws/emit', {
@@ -13,12 +13,15 @@ function enableLogging(enabled: boolean) {
     store.dispatch('ws/enableRemoteLogger', true);
 }
 
+we need a isRemoteLoggerEnabled function
+
 </script>
+
 
 
 <template>
     <div className="content p-0 p-sm-3">
-        <Toggle :minimal="false" :value="value" :valueOn="true" :valueoff="false"
+        <Toggle :minimal="false" :value="false" :valueOn="true" :valueoff="false"
             @update="(v: boolean) => enableLogging(v)">
         </Toggle>
     </div>
