702692ecd64f9809ba508f1a7260cba9e7d7b2d6
mikeathan <mikeathan@gmail.com>
2024-11-09 16:20:05 +0000

    wip

--- frontend/src/components/automations/Scheduler.vue
+++ frontend/src/components/automations/Scheduler.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { ref, watch, PropType } from "vue";
+import { ref, watch, PropType, computed } from "vue";
 import { getActionType, ActionType, } from "@/contracts/automations"
 import { AutomationTriggerAction } from "@/types/automation";
 import { emitClosePanel } from "@/mixins/useAutomationsEventBus";
 import { PanelComponents } from "@/mixins/usePanelComponents";
-
+import Dropdown from "@/components/controls/Dropdown.vue";
+import ButtonPanel from "@/components/controls/ButtonPanel.vue";
+import Selection from "@/components/input/Selection.vue";
+import { createSaveDeleteButtonItems } from "@/configs/automation/trigger-dropdown.config";
 
 const props = defineProps({
     id: String,
@@ -15,10 +18,25 @@ const emit = defineEmits<{
     (e: 'delete', action: AutomationTriggerAction): void,
 }>()
 
-
+const buttonPanelItems = computed(() => {
+  
+  return createSaveDeleteButtonItems(
+    () => saveAction(),
+    () => removeAction(),
+    !actionIsValid,
+    !actionIsValid
+  );
+});
 
 </script>
 <template>
+    div class="row">
+    <ButtonPanel :buttons="buttonPanelItems">
+      <Dropdown :items="dropdownItems" class-name="btn-light" :disabled="action.id == ''">
+        Add Operation
+      </Dropdown>
+    </ButtonPanel>
+  </div>
     <div>
 
         ID {{ props.id }}
