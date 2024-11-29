9a9f8d8300c6e4ae0a79231d00de0ac353b68566
mikeathan <mikeathan@gmail.com>
2024-11-29 16:37:24 +0000

    wip

--- frontend/src/components/automations/Trigger.vue
+++ frontend/src/components/automations/Trigger.vue
@@ -1,200 +1,304 @@
 <script setup lang="ts">
-import { computed, watch, ref, PropType } from "vue";
-import TriggerCondition from "./TriggerCondition.vue"
-import Selection from "../input/Selection.vue"
-import Dropdown from "../controls/Dropdown.vue"
-import { store } from "../../store/index";
-import { Device } from "@/types/device";
-import { AutomationTrigger, AutomationTriggerAction, AutomationTriggerCondition, AutomationTriggerConditions } from "@/types/automation";
-import { isValid, EditableTriggerCondition, EditableActionTrigger, ActionType } from "../../contracts/automations"
-import { capitalizeText } from "../../modules/formatters/text.formatter";
-import { EventActions, OpenPanelEvent } from "@/types/events.type";
-import { emitCloseLastPanel, emitClosePanel, emitOpenPanel } from "@/mixins/useAutomationsEventBus";
-import ActionViewer from "./actions/ActionViewer.vue";
-import ButtonPanel from "@/components/controls/ButtonPanel.vue";
-import { createButtons, createNewActionDropdownItems } from "../../configs/automation/trigger-dropdown.config";
+import { computed, watch, ref, PropType } from 'vue';
+import TriggerCondition from './TriggerCondition.vue';
+import Selection from '../input/Selection.vue';
+import Dropdown from '../controls/Dropdown.vue';
+import { store } from '../../store/index';
+import { Device } from '@/types/device';
+import {
+  AutomationTrigger,
+  AutomationTriggerAction,
+  AutomationTriggerCondition,
+  AutomationTriggerConditions,
+} from '@/types/automation';
+import {
+  isValid,
+  EditableTriggerCondition,
+  EditableActionTrigger,
+  ActionType,
+} from '../../contracts/automations';
+import { capitalizeText } from '../../modules/formatters/text.formatter';
+import {
+  EventActions,
+  OpenPanelEvent,
+} from '@/types/events.type';
+import {
+  emitCloseLastPanel,
+  emitClosePanel,
+  emitOpenPanel,
+} from '@/mixins/useAutomationsEventBus';
+import ActionViewer from './actions/ActionViewer.vue';
+import ButtonPanel from '@/components/controls/ButtonPanel.vue';
+import {
+  createButtons,
+  createNewActionDropdownItems,
+} from '../../configs/automation/trigger-dropdown.config';
 
 const props = defineProps({
-    id: { type: String },
-    trigger: { type: Object as PropType<AutomationTrigger>, default: {} as AutomationTrigger },
+  id: { type: String },
+  trigger: {
+    type: Object as PropType<AutomationTrigger>,
+    default: {} as AutomationTrigger,
+  },
 });
 
 // TODO: can be refactor to some automation context
-const conditions = ref<AutomationTriggerConditions>({} as AutomationTriggerConditions)
+const conditions = ref<AutomationTriggerConditions>(
+  {} as AutomationTriggerConditions,
+);
 const actions = ref<AutomationTriggerAction[]>([]); // have a list of actions with only 1 item capacity
 
-const trigger = ref<AutomationTrigger>(props.trigger)
+const trigger = ref<AutomationTrigger>(props.trigger);
 
-const dropDownitems = computed(() => createNewActionDropdownItems((e: ActionType) => addNewAction(e)));
-
+const dropDownitems = computed(() =>
+  createNewActionDropdownItems((e: ActionType) =>
+    addNewAction(e),
+  ),
+);
 
 const buttonPanelItems = computed(() => {
-    return createButtons([
-        {
-            name: "Save",
-            click: save,
-            disabled: isValid(trigger.value) == false
-        },
-        {
-            name: "Delete",
-            click: remove,
-            disabled: isValid(trigger.value) == false
-        }
-    ])
+  return createButtons([
+    {
+      name: 'Save',
+      click: save,
+      disabled: isValid(trigger.value) == false,
+    },
+    {
+      name: 'Delete',
+      click: remove,
+      disabled: isValid(trigger.value) == false,
+    },
+  ]);
 });
 
 watch(
-    () => props.trigger,
-    () => {
-        if (props.trigger.action != undefined && props.trigger.action.id != "") {
-            const action = JSON.parse(JSON.stringify(props.trigger.action)) as AutomationTriggerAction
-            actions.value.push(action);
-        }
-        conditions.value = JSON.parse(JSON.stringify(props.trigger.conditions)) as AutomationTriggerConditions;
-    }, { immediate: true }
-)
+  () => props.trigger,
+  () => {
+    if (
+      props.trigger.action != undefined &&
+      props.trigger.action.id != ''
+    ) {
+      const action = JSON.parse(
+        JSON.stringify(props.trigger.action),
+      ) as AutomationTriggerAction;
+      actions.value.push(action);
+    }
+    conditions.value = JSON.parse(
+      JSON.stringify(props.trigger.conditions),
+    ) as AutomationTriggerConditions;
+  },
+  { immediate: true },
+);
 
 const emit = defineEmits<{
-    (e: 'save', trigger: AutomationTrigger): void,
-    (e: 'delete', trigger: AutomationTrigger): void,
-}>()
-
+  (e: 'save', trigger: AutomationTrigger): void;
+  (e: 'delete', trigger: AutomationTrigger): void;
+}>();
 
 function save() {
+  trigger.value.conditions = conditions.value;
+  if (actions.value.length > 0) {
+    trigger.value.action = actions.value[0];
+  }
 
-    trigger.value.conditions = conditions.value
-    if (actions.value.length > 0) {
-        trigger.value.action = actions.value[0];
-    }
-
-    emit('save', trigger.value)
-    emitClosePanel('Trigger')
+  emit('save', trigger.value);
+  emitClosePanel('Trigger');
 }
 
 function remove() {
-    emit('delete', trigger.value)
-    emitClosePanel('Trigger')
+  emit('delete', trigger.value);
+  emitClosePanel('Trigger');
 }
 
 function addNewCondition() {
-    conditions.value.push(new EditableTriggerCondition());
+  conditions.value.push(new EditableTriggerCondition());
 }
 
-function removeTriggerCondition(condition: AutomationTriggerCondition) {
-    conditions.value = conditions.value.filter((c) => c != condition);
+function removeTriggerCondition(
+  condition: AutomationTriggerCondition,
+) {
+  conditions.value = conditions.value.filter(
+    (c) => c != condition,
+  );
 }
 
 const exposesList = computed(() => {
-    const device = store.getters["devices/find"](props.id) as Device;
-    if (device == null) {
-        return [];
-    }
-    return Object.assign({}, ...Object
-        .values(device.exposes)
-        .map((e) => ({ [e.name]: e.name })))
-})
+  const device = store.getters['devices/find'](
+    props.id,
+  ) as Device;
+  if (device == null) {
+    return [];
+  }
+  return Object.assign(
+    {},
+    ...Object.values(device.exposes).map((e) => ({
+      [e.name]: e.name,
+    })),
+  );
+});
 
 function deleteAction() {
-    actions.value = [];
+  actions.value = [];
 }
 
 function SaveAction(action: AutomationTriggerAction) {
-    actions.value[0] = action;
-    trigger.value.action = actions.value[0];
+  actions.value[0] = action;
+  trigger.value.action = actions.value[0];
 }
 
 function addNewAction(actionType: ActionType) {
-    emitOpenPanel(createActionOpenPanelEvent(new EditableActionTrigger(actionType), true));
+  emitOpenPanel(
+    createActionOpenPanelEvent(
+      new EditableActionTrigger(actionType),
+      true,
+    ),
+  );
 }
 
 const actionEvents = (): EventActions => {
-    return {
-        'delete': (e) => {
-            deleteAction()
-        },
-        'save': (a) => {
-            SaveAction(a)
-        }
-    }
+  return {
+    delete: (e) => {
+      deleteAction();
+    },
+    save: (a) => {
+      SaveAction(a);
+    },
+  };
 };
 
-function createActionOpenPanelEvent(action: AutomationTriggerAction, editMode: boolean): OpenPanelEvent {
-    return { name: 'ActionEditor', args: { automationId: props.id, item: action, editMode: editMode }, events: actionEvents() }
+function createActionOpenPanelEvent(
+  action: AutomationTriggerAction,
+  editMode: boolean,
+): OpenPanelEvent {
+  return {
+    name: 'ActionEditor',
+    args: {
+      automationId: props.id,
+      item: action,
+      editMode: editMode,
+    },
+    events: actionEvents(),
+  };
 }
-
 </script>
 
 <template>
+  <!-- TODO:  -->
+  <!-- if automation for device exists message user else we overwrite it -->
 
-    <!-- TODO:  -->
-    <!-- if automation for device exists message user else we overwrite it -->
-
-    <div class="row pb-3">
-        <div class="col">
-            <ButtonPanel :buttons="buttonPanelItems">
-                <Dropdown :items="dropDownitems" class-name="btn-light" :disabled="actions.length != 0">
-                    New Action
-                </Dropdown>
-            </ButtonPanel>
-        </div>
+  <div class="row pb-3">
+    <div class="col">
+      <ButtonPanel :buttons="buttonPanelItems" />
     </div>
-    <div class="row" v-if="trigger.name == ''">
-        <Selection :value="trigger.name" text="Select trigger" :disabled="trigger.name != ''" size="normal"
-            @updated="v => trigger.name = v" :items="exposesList">
-        </Selection>
-    </div>
-    <div class="row" v-else>
-        <h5>Trigger for {{ capitalizeText(trigger.name) }}</h5>
+  </div>
+  <div class="row" v-if="trigger.name == ''">
+    <Selection
+      :value="trigger.name"
+      text="Select trigger"
+      :disabled="trigger.name != ''"
+      size="normal"
+      @updated="(v) => (trigger.name = v)"
+      :items="exposesList">
+    </Selection>
+  </div>
+  <div class="row" v-else>
+    <h5>Trigger for {{ capitalizeText(trigger.name) }}</h5>
 
-        <Fieldset legend="When" :toggleable="true" :collapsed="true">
-            <DataTable :value="conditions" tableStyle="min-width: 50rem" selectionMode="single">
-                <Column header="Condition">
-                    <template #body="slotProps">
-                        <TriggerCondition :id="props.id" :name="slotProps.data.name" :operator="slotProps.data.equality"
-                            :data="slotProps.data.value" @update:name="newValue => slotProps.data.name = newValue"
-                            @update:value="newValue => slotProps.data.value = newValue"
-                            @update:operator="newValue => slotProps.data.equality = newValue">
-                        </TriggerCondition>
-                    </template>
-                </Column>
-                <Column>
-                    <template #header="slotProps">
-                        <Button icon="pi pi-plus" variant="text" rounded @click="addNewCondition" />
-                    </template>
-                    <template #body="slotProps">
-                        <Button icon="pi pi-trash" variant="text" rounded @click="
-                            removeTriggerCondition(slotProps.data)
-                            " />
-                    </template>
-                </Column>
-            </DataTable>
-        </Fieldset>
+    <Fieldset
+      legend="When"
+      :toggleable="true"
+      :collapsed="true">
+      <DataTable
+        :value="conditions"
+        tableStyle="min-width: 50rem"
+        selectionMode="single">
+        <Column header="Condition">
+          <template #body="slotProps">
+            <TriggerCondition
+              :id="props.id"
+              :name="slotProps.data.name"
+              :operator="slotProps.data.equality"
+              :data="slotProps.data.value"
+              @update:name="
+                (newValue) =>
+                  (slotProps.data.name = newValue)
+              "
+              @update:value="
+                (newValue) =>
+                  (slotProps.data.value = newValue)
+              "
+              @update:operator="
+                (newValue) =>
+                  (slotProps.data.equality = newValue)
+              ">
+            </TriggerCondition>
+          </template>
+        </Column>
+        <Column>
+          <template #header="slotProps">
+            <!-- <Button
+              icon="pi pi-plus"
+              variant="text"
+              rounded
+              @click="addNewCondition" /> -->
+          </template>
+          <template #body="slotProps">
+            <Button
+              icon="pi pi-trash"
+              variant="text"
+              rounded
+              @click="
+                removeTriggerCondition(slotProps.data)
+              " />
+          </template>
+        </Column>
+      </DataTable>
+      Add new condtion button
+    </Fieldset>
 
-        <Fieldset legend="Then" :toggleable="true" :collapsed="false">
-            <DataTable :value="actions" tableStyle="min-width: 50rem" selectionMode="single">
-                <Column header="Actions">
-                    <template #body="slotProps">
-                        <ActionViewer :automation-id="props.id" :item="slotProps.data" :edit-events="actionEvents()"
-                            @delete="deleteAction()">
-                        </ActionViewer>
+    <div class="pt-2"></div>
+    <Fieldset
+      legend="Then"
+      :toggleable="true"
+      :collapsed="false">
+      <DataTable
+        :value="actions"
+        tableStyle="min-width: 50rem"
+        selectionMode="single"
+        fixedHeader="1,2">
+        <Column header="Actions" >
+          <template #body="slotProps">
+            <ActionViewer
+              :automation-id="props.id"
+              :item="slotProps.data"
+              :edit-events="actionEvents()"
+              @delete="deleteAction()">
+            </ActionViewer>
+          </template>
+        </Column>
+        <Column >
+          <!-- <template #header="slotProps">
+            <Dropdown
+              :items="dropDownitems"
+              :disabled="actions.length != 0"
+              text
+              icon="pi pi-plus"
+              size="small" />
+          </template> -->
+          <template #body="slotProps">
+            <Button
+              icon="pi pi-trash"
+              variant="text"
+              rounded
+              @click="deleteAction()" />
+          </template>
+        </Column>
+      </DataTable>
 
-                        maybe use dataaview
-                    </template>
-                </Column>
-                <Column>
-                    <template #header="slotProps">
-                        <SplitButton text icon="pi pi-plus" size="small"></SplitButton>
-                    </template>
-                    <template #body="slotProps">
-                        <Button icon="pi pi-trash" variant="text" rounded @click="
-                            deleteAction()
-                            " />
-                    </template>
-                </Column>
-            </DataTable>
-        </Fieldset>
-    </div>
-    <!--     
+      Add new Action dropdown
+    </Fieldset>
+  </div>
+  <!--     
     <div class="row pb-3">
         <div class="col">
             <ButtonPanel :buttons="buttonPanelItems">
--- frontend/src/components/controls/Dropdown.vue
+++ frontend/src/components/controls/Dropdown.vue
@@ -1,33 +1,64 @@
 <script setup lang="ts">
-import { PropType } from "vue";
-import { DropDownItemType } from "../../types/controls.type";
+import { PropType } from 'vue';
+import { DropDownItemType } from '../../types/controls.type';
 
 const props = defineProps({
-    items: {
-        type: Object as PropType<Array<DropDownItemType>>,
-        default: [],
-        required: true
-    },
-    className: {
-        type: String,
-        default: ""
-    },
-    disabled: {
-        type: Boolean,
-        default: false,
-    }
+  items: {
+    type: Object as PropType<Array<DropDownItemType>>,
+    default: [],
+    required: true,
+  },
+  size: {
+    type: String as PropType<'small' | 'large' | undefined>,
+    default: 'small',
+  },
+  icon: {
+    type: String,
+    default: '',
+  },
+  className: {
+    type: String,
+    default: '',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
 });
 
+function convert() {
+  return props.items.map((item) => {
+    return {
+      label: item.name,
+      command: () => item.click(item.value),
+    };
+  });
+}
 </script>
 <template>
-    <button :id="`dropdownControl`" type="button" :class="`btn ${props.className}`" data-bs-toggle="dropdown"
-        aria-expanded="false" :disabled="props.disabled">
-        <slot></slot>
-    </button>
-    <ul class="dropdown-menu">
-        <li v-for="item in props.items" :key="item.value">
-            <a @click="item.event(item.value)" class="dropdown-item" data-toggle="dropdown">
-                {{ item.name }}</a>
-        </li>
-    </ul>
+  <SplitButton
+    :model="convert()"
+    :disabled="props.disabled"
+    text 
+    :icon="props.icon" :size="props.size"/>
+  
+  <!-- <button
+    :id="`dropdownControl`"
+    type="button"
+    :class="`btn ${props.className}`"
+    data-bs-toggle="dropdown"
+    aria-expanded="false"
+    :disabled="props.disabled">
+    <slot></slot>
+  </button>
+  <ul class="dropdown-menu">
+    <li v-for="item in props.items" :key="item.value">
+      <a
+        @click="item.click(item.value)"
+        class="dropdown-item"
+        data-toggle="dropdown">
+        {{ item.name }}</a
+      >
+    </li>
+  </ul> -->
 </template>
