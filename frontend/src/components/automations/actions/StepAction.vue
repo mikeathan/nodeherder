3df6f95fc74a3cac4d2c6a47c42129c1eb906abe
mikeathan <mikeathan@gmail.com>
2024-12-01 20:13:50 +0000

    wip

--- frontend/src/components/automations/actions/StepAction.vue
+++ frontend/src/components/automations/actions/StepAction.vue
@@ -117,72 +117,62 @@ function deviceSelected(deviceId: string, friendlyName: string) {
   <!-- action controls -->
   <div class="row pb-3">
     <div class="col">
-      <ButtonPanel :buttons="buttonPanelItems">
-        <Dropdown :items="dropdownItems" class-name="btn-light" :disabled="action.id == ''">
-          Add Operation
-        </Dropdown>
-      </ButtonPanel>
+      <ButtonPanel :buttons="buttonPanelItems" />
     </div>
   </div>
 
   <h4>Step Action</h4>
   <div class="pb-3" />
 
-
   <!-- device select box  -->
   <div class="row pb-2">
     <DeviceSelector label="Device to trigger" @updated="deviceSelected" :id="action.id"
-      :filter="featureDevicesFilter()">
-    </DeviceSelector>
+      :filter="featureDevicesFilter()" />
   </div>
 
   <!-- data input box  -->
   <div class="row ">
     <div class="col-sm-3">
-      <InputBox label="Set value" :is-numeric="true" :value="action.data" @updated="actionDataChanged">
-      </InputBox>
+      <InputBox label="Set value" :is-numeric="true" :value="action.data" @updated="actionDataChanged" />
     </div>
   </div>
-
+  <div class="flex align-items-center justify-content-left pt-3">
+    <Dropdown :items="dropdownItems" :disabled="action.id == ''" text label="Operations" icon="pi pi-plus"
+      size="small" />
+  </div>
   <!-- Steps -->
-  <div v-if="action.steps.length > 0" class="card">
+  <div v-if="action.steps.length > 0">
 
-    <div class="card-header">
-      <h5>Operations</h5>
-    </div>
-    <div class="card-body">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item" v-for="step in action.steps">
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-2">
-                <Selection :value="step.operator" :items="NumericOperators" @updated="o => step.operator = o">
-                </Selection>
-              </div>
-              <!-- step id  -->
-              <div class="col col-xl-4" v-if="step.id == ''">
-                <DeviceSelector @updated="(id, name) => stepDeviceSelected(id, name, step)"
-                  :filter="devicesFilterByActionStep(props.automationId, action, step)"></DeviceSelector>
-              </div>
-              <div v-else class="col col-xl-3">
-                {{ deviceNameFromId(step) }}
-              </div>
-              <!-- step property -->
-              <div class="col col-xl-6">
-                <ExposeSelector :id="step.id" @updated="(v) => stepPropertySelected(v, step)" :value="step.property"
-                  :filter="exposeFilterByType(ExposeTypes.Numeric)" position="center"></ExposeSelector>
-              </div>
-              <div class="col-md-1 col-sm-1">
-                <div class="d-grid d-md-auto">
-                  <a class="btn btn-sm  btn-light " role="button">
-                    <span class="fa fa-trash-alt fa-sm" @click="removeStep(step)"></span>
-                  </a>
-                </div>
-              </div>
-            </div>
+    <DataTable :value="action.steps">
+      <Column header="Operator">
+        <template #body="slotProps">
+          <Selection :value="slotProps.data.operator" :items="NumericOperators"
+            @updated="o => slotProps.data.operator = o" />
+        </template>
+      </Column>
+
+      <Column header="Device">
+        <template #body="slotProps">
+          <div v-if="slotProps.data.id == ''" style="display: flex; align-items: center; gap: 0.5rem;">
+            <DeviceSelector @updated="(id, name) => stepDeviceSelected(id, name, slotProps.data)"
+              :filter="devicesFilterByActionStep(props.automationId, action, slotProps.data)"></DeviceSelector>
           </div>
-        </li>
-      </ul>
-    </div>
+          <div v-else style="display: flex; align-items: center; gap: 0.5rem;">
+            {{ deviceNameFromId(slotProps.data) }}
+          </div>
+        </template>
+      </Column>
+
+
+      <Column header="Expose">
+        <template #body="slotProps">
+          <div style="display: flex; align-items: center; gap: 0.5rem;">
+            <ExposeSelector :id="slotProps.data.id" @updated="(v) => stepPropertySelected(v, slotProps.data)"
+              :value="slotProps.data.property" :filter="exposeFilterByType(ExposeTypes.Numeric)" />
+            <Button icon="pi pi-trash" text iconOnly="true" @click="removeStep(slotProps.data)" />
+          </div>
+        </template>
+      </Column>
+    </DataTable>
   </div>
 </template>
