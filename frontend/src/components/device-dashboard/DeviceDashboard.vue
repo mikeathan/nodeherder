24fb066f6a0396cbaa0d2d99e490a0fdd2c7737c
mikeathan <mikeathan@gmail.com>
2024-12-18 20:21:15 +0000

    wip

--- frontend/src/components/device-dashboard/DeviceDashboard.vue
+++ frontend/src/components/device-dashboard/DeviceDashboard.vue
@@ -9,11 +9,16 @@ const devices = computed(
 );
 
 </script>
-<style scoped></style>
+<style scoped>
+
+.dashboard {
+  background-color: var(--surface-card); /* Use the same background color as PrimeVue cards */
+    height: 100vh; /* Optional: Full height */
+    overflow: auto; /* Optional: Scroll if content exceeds */}</style>
 
 <template>
 
-    <div class="grid ">
+    <div class="grid dashboard" >
       <div class="col-12 md:col-6 lg:col-3 xg:col-2" v-for="device in devices" :key="device.id">
         <DeviceCard :device="device" :key="device.id"></DeviceCard>
       </div>
