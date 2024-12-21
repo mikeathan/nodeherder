a466617329a563ad9a0e5618388579b0573df36a
mikeathan <mikeathan@gmail.com>
2024-12-21 19:23:30 +0000

    set mqtt ip

--- core/main.go
+++ core/main.go
@@ -58,7 +58,7 @@ func main() {
 	mqttConfig := mqtt.MqttConfig{
 		Username:   "sinkhole",
 		Password:   "mqtt2023",
-		Broker:     "tcp://localhost:1883",
+		Broker:     "tcp://192.168.50.241:1883",
 		ClientType: args.buildType,
 	}
 
