68a67c49e54b9d70c49d03abe9350078fc4edf52
mikeathan <mikeathan@gmail.com>
2024-12-29 09:04:11 +0000

    updatng eslint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 4100,
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
