e4003007a762e339ec76b2e5d86bc0b990a60a61
mikeathan <mikeathan@gmail.com>
2024-12-31 14:28:28 +0000

    fix toast position

--- frontend/src/components/hub/alerts/Notifications.vue
+++ frontend/src/components/hub/alerts/Notifications.vue
@@ -1,35 +1,35 @@
 <script setup lang="ts">
-  import { computed, ref, watch, watchEffect } from 'vue';
-  import { store } from '../../../store/index';
-  import { AlertMessage } from '../../../types/alerts.type';
-  import Toast from 'primevue/toast';
-  import { useToast } from 'primevue/usetoast';
-  import { toToastSeverity } from '@/contracts/alerts';
+import { computed, ref, watch, watchEffect } from 'vue';
+import { store } from '../../../store/index';
+import { AlertMessage } from '../../../types/alerts.type';
+import Toast from 'primevue/toast';
+import { useToast } from 'primevue/usetoast';
+import { toToastSeverity } from '@/contracts/alerts';
 
-  const toast = useToast();
+const toast = useToast();
 
-  watchEffect(() => {
-    const messages = store.getters[
-      'alerts/messages'
-    ]() as AlertMessage[];
+watchEffect(() => {
+  const messages = store.getters[
+    'alerts/messages'
+  ]() as AlertMessage[];
 
-    messages.forEach((alert: AlertMessage) => {
-      if (alert.visible) return;
-      alert.visible = true;
-      toast.add({
-        severity: toToastSeverity(
-          alert.severity!
-        ) as typeof ToastMessageOptions, // TODO - fix
-        summary: alert.title,
-        detail: alert.message,
-        life: alert.timeout,
-      });
+  messages.forEach((alert: AlertMessage) => {
+    if (alert.visible) return;
+    alert.visible = true;
+    toast.add({
+      // @ts-ignore
+      severity: toToastSeverity(
+        alert.severity!
+      ),
+      summary: alert.title,
+      detail: alert.message,
+      life: alert.timeout,
     });
   });
+});
 </script>
+<style scoped></style>
 
 <template>
-  <div class="text-center ma-2">
-    <Toast />
-  </div>
+  <Toast position="top-right" />
 </template>
