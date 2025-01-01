69e2c01ab00b6642f0ab58d215f0542947405427
mikeathan <mikeathan@gmail.com>
2025-01-01 20:05:34 +0000

    remove device dialog

--- frontend/src/components/device/DeviceAbout.vue
+++ frontend/src/components/device/DeviceAbout.vue
@@ -1,94 +1,98 @@
 <script setup lang="ts">
-  import { store } from '../../store/index';
-  import { computed, ref } from 'vue';
-  import LastSeen from '../device/LastSeen.vue';
-  import PowerSource from '../device/PowerSource.vue';
-  import ConnectionType from '../device/ConnectionType.vue';
-  import RenameDeviceDialog from '../dialogs/RenameDeviceDialog.vue';
-  import ConfirmDialog from '../dialogs/ConfirmDialog.vue';
+import { store } from '../../store/index';
+import { computed, ref } from 'vue';
+import LastSeen from '../device/LastSeen.vue';
+import PowerSource from '../device/PowerSource.vue';
+import ConnectionType from '../device/ConnectionType.vue';
+import RenameDeviceDialog from '../dialogs/RenameDeviceDialog.vue';
+import ConfirmDialog from '../dialogs/ConfirmDialog.vue';
+import RemoveDeviceDialog from '../dialogs/RemoveDeviceDialog.vue';
+import { Device } from '@/types/device';
 
-  import { Device } from '@/types/device';
+const props = defineProps({
+  id: String,
+});
 
-  const props = defineProps({
-    id: String,
+const showRenameDialog = ref(false);
+const showInterviewDialog = ref(false);
+const showRemoveDialog = ref(false);
+
+const device = computed(() => {
+  return store.getters['devices/find'](props.id);
+});
+
+function renameDevice(value: string) {
+  store.dispatch('devices/rename', {
+    name: device.value.friendly_name,
+    newName: value,
   });
+}
 
-  const showRenameDialog = ref(false);
-  const showInterviewDialog = ref(false);
-  
-  const device = computed(() => {
-    return store.getters['devices/find'](props.id);
+function interviewDevice() {
+  store.dispatch('devices/interview', {
+    id: device.value.id,
   });
+}
 
-  function renameDevice(value: string) {
-    store.dispatch('devices/rename', {
-      name: device.value.friendly_name,
-      newName: value,
-    });
+function removeDevice(event: string) {
+  console.log('removeDevice', event);
+  store.dispatch('devices/remove', {
+    id: device.value.id,
+  });
+}
+const displayProps = computed(() => {
+  const device = store.getters['devices/find'](
+    props.id
+  ) as Device;
+  if (device == undefined) {
+    return [];
   }
 
-  function interviewDevice() {
-    store.dispatch('devices/interview', {
-      id: device.value.id,
-    });
-  }
-
-  const displayProps = computed(() => {
-    const device = store.getters['devices/find'](
-      props.id
-    ) as Device;
-    if (device == undefined) {
-      return [];
-    }
-
-    return [
-      {
-        key: 'IEEE Address:',
-        value: device.id,
+  return [
+    {
+      key: 'IEEE Address:',
+      value: device.id,
+    },
+    {
+      key: 'Friendly name:',
+      value: device.friendly_name,
+    },
+    {
+      key: 'Description:',
+      value: device.description,
+    },
+    {
+      key: 'Availability:',
+      value: device.properties.availability,
+    },
+    {
+      key: 'Last seen:',
+      type: LastSeen,
+      props: {
+        timestamp: device.properties.last_seen,
       },
-      {
-        key: 'Friendly name:',
-        value: device.friendly_name,
+    },
+    {
+      key: 'Power source:',
+      type: PowerSource,
+      props: {
+        power_source: device.power_source,
+        value: device.properties.battery,
       },
-      {
-        key: 'Description:',
-        value: device.description,
+    },
+    {
+      key: 'Connection Type:',
+      type: ConnectionType,
+      props: {
+        type: device.connection_type,
       },
-      {
-        key: 'Availability:',
-        value: device.properties.availability,
-      },
-      {
-        key: 'Last seen:',
-        type: LastSeen,
-        props: {
-          timestamp: device.properties.last_seen,
-        },
-      },
-      {
-        key: 'Power source:',
-        type: PowerSource,
-        props: {
-          power_source: device.power_source,
-          value: device.properties.battery,
-        },
-      },
-      {
-        key: 'Connection Type:',
-        type: ConnectionType,
-        props: {
-          type: device.connection_type,
-        },
-      },
-    ];
-  });
+    },
+  ];
+});
 </script>
 <template>
   <div>
-    <dl
-      class="grid grid-nogutter"
-      v-for="(prop, idx) in displayProps"
-      :key="idx">
+    <dl class="grid grid-nogutter" v-for="(prop, idx) in displayProps" :key="idx">
       <dt class="col-12 md:col-5 text-secondary">
         {{ prop.key }}
       </dt>
@@ -97,31 +101,19 @@
           <span title="last update">{{ prop.value }}</span>
         </div>
         <template v-else>
-          <component
-            :is="prop.type"
-            v-bind="prop.props"></component>
+          <component :is="prop.type" v-bind="prop.props"></component>
         </template>
       </dd>
     </dl>
   </div>
-  <Button
-    icon="pi pi-user-edit"
-    variant="text"
-    v-tooltip="'Rename device'"
-    @click="showRenameDialog = true" />
-  <RenameDeviceDialog
-    :friendlyName="device.friendly_name"
-    :show="showRenameDialog"
-    @update:name="renameDevice"
+  <Button icon="pi pi-user-edit" variant="text" v-tooltip="'Rename device'" @click="showRenameDialog = true" />
+  <RenameDeviceDialog :friendlyName="device.friendly_name" :show="showRenameDialog" @update:name="renameDevice"
     @close="showRenameDialog = false" />
 
-  <Button
-    icon="pi pi-sync"
-    variant="text"
-    v-tooltip="'Interview device'"
-    @click="showInterviewDialog = true" />
-  <ConfirmDialog
-    :show="showInterviewDialog"
-    @confirm="interviewDevice"
-    @close="showInterviewDialog = false" />
+  <Button icon="pi pi-sync" variant="text" v-tooltip="'Interview device'" @click="showInterviewDialog = true" />
+  <ConfirmDialog :show="showInterviewDialog" @confirm="interviewDevice" @close="showInterviewDialog = false" />
+
+  <Button icon="pi pi-trash" variant="text" v-tooltip="'Remove device'" @click="showRemoveDialog = true" />
+  <RemoveDeviceDialog :friendlyName="device.friendly_name" :show="showRemoveDialog" @remove="e => removeDevice(e)"
+    @close="showRemoveDialog = false" />
 </template>
