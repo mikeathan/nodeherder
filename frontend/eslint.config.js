2900faf054b77c952383cef6b7f485adb478469c
mikeathan <mikeathan@gmail.com>
2025-01-02 19:13:34 +0000

    remove airbnb eslinter

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,6 @@
 import vue from 'eslint-plugin-vue';
 import vueParser from 'vue-eslint-parser';
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
 
-const airbnbBase = require('eslint-config-airbnb-base');
 export default [
   {
     ignores: ['dist'], // Exclude the 'dist' folder
@@ -11,12 +8,10 @@ export default [
   {
     plugins: {
       vue,
-      ...airbnbBase.plugins,
     },
     languageOptions: {
       parser: vueParser,
       parserOptions: {
-        ...airbnbBase.parserOptions,
         ecmaVersion: 2020,
         sourceType: 'module',
         ecmaFeatures: {
@@ -34,18 +29,14 @@ export default [
       vue: {
         version: 'detect', // Automatically detect Vue version
       },
-      ...airbnbBase.settings,
     },
-
     rules: {
       ...vue.configs.recommended.rules, // Vue recommended rules
-      ...airbnbBase.rules,
       'no-unused-vars': 'warn',
       'no-console': 'warn',
       'indent': ['error', 2],
       'quotes': ['error', 'single'],
       'semi': ['error', 'always'],
-      'import/no-unresolved': 'off',
     },
   },
   {
