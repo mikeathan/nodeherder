e769870e1ef1d391d32fa46b93b36493640677d1
mikeathan <mikeathan@gmail.com>
2025-01-19 19:43:16 +0000

    todo

--- frontend/src/components/device/DeviceSettings.vue
+++ frontend/src/components/device/DeviceSettings.vue
@@ -1,34 +1,36 @@
 <script setup lang="ts">
-  import { store } from '../../store/index';
-  import { computed } from 'vue';
-  import Toggle from '../input/Toggle.vue';
-  import { DeviceSettings } from '@/types/settings';
-  import InputBox from '../input/InputBox.vue';
+import { store } from '../../store/index';
+import { computed } from 'vue';
+import Toggle from '../input/Toggle.vue';
+import { DeviceSettings } from '@/types/settings';
+import InputBox from '../input/InputBox.vue';
 
-  const props = defineProps({
-    id: { type: String, required: true },
-  });
+const props = defineProps({
+  id: { type: String, required: true },
+});
 
-  const deviceSettings = computed(() => {
-    return store.getters['hub/findDeviceSetting'](props.id);
-  });
+const deviceSettings = computed(() => {
+  return store.getters['hub/findDeviceSetting'](props.id);
+});
 
-  function toggleChanged(propName: any, propValue: any) {
-    save(propName, propValue);
+function toggleChanged(propName: any, propValue: any) {
+  save(propName, propValue);
+}
+
+function inputLostFocus(propName: any, propValue: any) {
+  console.log('inputLostFocus', propName, propValue);
+  save(propName, propValue);
+}
+
+function save(propName: any, propValue: any) {
+  if (deviceSettings.value[propName] != propValue) {
+    deviceSettings.value[propName] = propValue;
+    store.dispatch('hub/saveDeviceSettings', deviceSettings.value as DeviceSettings);
   }
-
-  function inputLostFocus(propName: any, propValue: any) {
-    console.log('inputLostFocus', propName, propValue);
-    save(propName, propValue);
-  }
-
-  function save(propName: any, propValue: any) {
-    if (deviceSettings.value[propName] != propValue) {
-      deviceSettings.value[propName] = propValue;
-      store.dispatch('hub/saveDeviceSettings', deviceSettings.value as DeviceSettings);
-    }
-  }
+}
 </script>
+
+to fix the duration , give it vlaue as it doesnt make sense when tis 500000
 <template>
   <div class="grid col-12 align-items-center grid-nogutter" v-for="(value, key) in deviceSettings" :key="key">
     <dl class="col-12 md:col-3">
@@ -38,19 +40,12 @@
     </dl>
     <div class="md:col-4">
       <div v-if="typeof value === 'boolean'">
-        <Toggle
-          :minimal="false"
-          :value="value"
-          :valueOn="true"
-          :valueoff="false"
+        <Toggle :minimal="false" :value="value" :valueOn="true" :valueoff="false"
           @update="(v) => toggleChanged(key, v)">
         </Toggle>
       </div>
       <div v-else>
-        <InputBox
-          :value="value"
-          :disabled="typeof value !== 'number'"
-          :is-numeric="typeof value === 'number'"
+        <InputBox :value="value" :disabled="typeof value !== 'number'" :is-numeric="typeof value === 'number'"
           @lost-focus="(f) => inputLostFocus(key, f)">
         </InputBox>
       </div>
