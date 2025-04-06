bf9ae1f0f838ee13e65051d4d6226d5ff12e7271
mikeathan <mikeathan@gmail.com>
2025-04-06 20:06:05 +0100

    wip

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -8,6 +8,9 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Accordion: typeof import('primevue/accordion')['default']
+    AccordionContent: typeof import('primevue/accordioncontent')['default']
+    AccordionHeader: typeof import('primevue/accordionheader')['default']
+    AccordionPanel: typeof import('primevue/accordionpanel')['default']
     AccordionTab: typeof import('primevue/accordiontab')['default']
     ActionEditor: typeof import('./src/components/automations/actions/ActionEditor.vue')['default']
     ActionViewer: typeof import('./src/components/automations/actions/ActionViewer.vue')['default']
