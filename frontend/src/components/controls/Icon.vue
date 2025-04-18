e2e45cc2f2218bad057a3ceb88df0ae8e1d36693
mikeathan <mikeathan@gmail.com>
2025-04-18 19:26:12 +0100

    wip

--- frontend/src/components/controls/Icon.vue
+++ frontend/src/components/controls/Icon.vue
@@ -1,39 +1,48 @@
 <script setup lang="ts">
-import { PropType } from 'vue';
-import { IconProps } from '../../types/icon.type';
+  import { PropType } from 'vue';
+  import { IconProps } from '../../types/icon.type';
 
-const props = defineProps({
-  icon: {
-    type: Object as PropType<IconProps>,
-    default: {} as IconProps,
-    required: true,
-  },
-  rotationAngle: {
-    type: String,
-    default: '0',
-    required: false,
-  },
-  width: {
-    type: String,
-    default: '20px',
-  },
-  height: {
-    type: String,
-    default: '20px',
-  },
-});
-
+  const props = defineProps({
+    icon: {
+      type: Object as PropType<IconProps>,
+      default: {} as IconProps,
+      required: true,
+    },
+    rotationAngle: {
+      type: String,
+      default: '0',
+      required: false,
+    },
+    width: {
+      type: String,
+      default: '20px',
+    },
+    height: {
+      type: String,
+      default: '20px',
+    },
+    circleColor: {
+      type: String,
+      default: '#ccc',
+    },
+  });
 </script>
 <style scoped>
-/* Temporary */
-.align-middle {
-  margin-right: 0.2em;
-}
+  /* Temporary */
+  .align-middle {
+    margin-right: 0.2em;
+  }
 </style>
 <template>
-  <svg :width="width" :height="height" viewBox="0 0 22 22" :style="{ fill: props.icon.color }"
-    :aria-label="props.icon.tooltip" class="align-middle">
+  <svg
+    :width="width"
+    :height="height"
+    viewBox="0 0 22 22"
+    :style="{ fill: props.icon.color }"
+    :aria-label="props.icon.tooltip"
+    class="align-middle">
     <title>{{ props.icon.tooltip }}</title>
+    <!-- <circle cx="11" cy="11" r="11" stroke="currentColor" fill="none" stroke-width="1" /> -->
     <g :transform="`rotate(${rotationAngle} 12 12)`">
       <path :d="props.icon.name" />
     </g>
--- frontend/src/components/expose/brightness.vue
+++ frontend/src/components/expose/brightness.vue
@@ -1,70 +1,83 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import { prop } from 'vue-class-component';
-const props = defineProps({
-  value: {
-    type: Number,
-    default: 50,
-  },
-});
-const emit = defineEmits<{
-  (e: 'update', value: number): void;
-}>();
+  import { ref, computed, watch } from 'vue';
+  import { prop } from 'vue-class-component';
+  const props = defineProps({
+    value: {
+      type: Number,
+      default: 50,
+    },
+    min: {
+      type: Number,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      default: 100,
+    },
+  });
+  const emit = defineEmits<{
+    (e: 'update', value: number): void;
+  }>();
 
-const value = ref(props.value);
+  const value = ref(props.value);
 
-const trackFill = computed(() => {
-  const percent = value.value;
-  return {
-    background: `linear-gradient(to right, #ffc107 ${percent}%, #fff4cc ${percent}%)`,
-  };
-});
-
-function updateValue(event: any): void {
-  const newValue = parseInt(event.target.value);
-  emit('update', newValue);
-}
+  const trackFill = computed(() => {
+    const percent = ((value.value - props.min) / (props.max - props.min)) * 100;
+    return {
+      background: `linear-gradient(to right, #ffc107 ${percent}%, #fff4cc ${percent}%)`,
+    };
+  });
+  watch(
+    () => props.value,
+    (newVal) => {
+      value.value = newVal;
+    }
+  );
+  function updateValue(event: any): void {
+    const newValue = parseInt(event.target.value);
+    emit('update', newValue);
+  }
 </script>
 
 <template>
   <div class="slider-wrapper">
     <div class="track-background" :style="trackFill"></div>
-    <input type="range" min="0" max="100" v-model="value" class="slider" @change="updateValue" />
+    <input type="range" :min="props.min" :max="props.max" v-model="value" class="slider" @change="updateValue" />
   </div>
 </template>
 
 <style scoped>
-.slider-wrapper {
-  position: relative;
-  width: 280px;
-  height: 50px;
-  overflow: hidden;
-}
+  .slider-wrapper {
+    position: relative;
+    width: 280px;
+    height: 50px;
+    overflow: hidden;
+  }
 
-.track-background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  border-radius: 10px / 10px;
-  pointer-events: none;
-}
+  .track-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 10px / 10px;
+    pointer-events: none;
+  }
 
-/* input layer */
-.slider {
-  appearance: none;
-  width: 100%;
-  height: 100%;
-  background: transparent;
-  position: relative;
-  z-index: 1;
-  cursor: pointer;
-  outline: none;
-}
+  /* input layer */
+  .slider {
+    appearance: none;
+    width: 100%;
+    height: 100%;
+    background: transparent;
+    position: relative;
+    z-index: 1;
+    cursor: pointer;
+    outline: none;
+  }
 
-/* Webkit thumb */
-/* .slider::-webkit-slider-thumb {
+  /* Webkit thumb */
+  /* .slider::-webkit-slider-thumb {
   appearance: none;
   width: 32px;
   height: 6px;
@@ -77,21 +90,21 @@ function updateValue(event: any): void {
   box-shadow: 0 0 1px rgba(0, 0, 0, 0.2);
 } */
 
-/* Firefox track & thumb */
-.slider::-moz-range-track {
-  height: 100%;
-  background: transparent;
-  border: none;
-}
+  /* Firefox track & thumb */
+  .slider::-moz-range-track {
+    height: 100%;
+    background: transparent;
+    border: none;
+  }
 
-.slider::-moz-range-thumb {
-  width: 6px;
-  height: 30px;
-  background: white;
-  border: none;
-  border-radius: 3px;
-  cursor: pointer;
-  transform: translateX(-10px);
-  /* Shift left by 2 pixels */
-}
+  .slider::-moz-range-thumb {
+    width: 6px;
+    height: 30px;
+    background: white;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+    transform: translateX(-10px);
+    /* Shift left by 2 pixels */
+  }
 </style>
