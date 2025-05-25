fc7c5f36b5bbbccfbe5946d6fb7f9b0214f040a8
mikeathan <mikeathan@gmail.com>
2025-05-25 20:32:47 +0100

    stying

--- frontend/src/modules/formatters/sensor-formatter.ts
+++ frontend/src/modules/formatters/sensor-formatter.ts
@@ -167,48 +167,6 @@ const typeToClassMapsensor: KeyValuePair<IconProps> = {
   child_lock: { name: mdiLockOutline, color: 'white' },
 };
 
-// const typeToClassMapsensor: KeyValuePair<string> = {
-//   humidity: 'pi pi-droplet',
-//   illuminance: 'pi pi-sun',
-//   pressure: 'pi pi-cloud-download',
-//   co2: 'mdi mdi-molecule-co2',
-//   voltage: 'pi pi-bolt',
-//   state: 'pi pi-star-half',
-//   brightness: 'pi pi-sun',
-//   occupancy: 'mdi mdi-walk',
-//   current: 'mdi mdi-current-ac',
-//   power: 'pi pi-power-off',
-//   energy: 'mdi mdi-lightning-bolt',
-//   frequency: 'mdi mdi-sine-wave',
-//   tamper: 'pi pi-exclamation-triangle',
-//   smoke: 'mdi mdi-smoke',
-//   radiation_dose_per_hour: 'mdi mdi-radiation',
-//   radioactive_events_per_minute: 'mdi mdi-radioactive',
-//   power_factor: 'mdi mdi-chart-line',
-//   mode: 'pi pi-cog',
-//   sound: 'pi pi-volume-up',
-//   position: 'mdi mdi-map-marker',
-//   alarm: 'pi pi-bell',
-//   color_xy: 'mdi mdi-palette',
-//   color_hs: 'mdi mdi-palette',
-//   color_temp: 'mdi mdi-thermometer',
-//   illuminance_lux: 'pi pi-sun',
-//   soil_moisture: 'mdi mdi-water-percent',
-//   water_leak: 'mdi mdi-leak',
-//   week: 'pi pi-calendar-clock',
-//   workdays_schedule: 'pi pi-calendar',
-//   holidays_schedule: 'pi pi-calendar-times',
-//   away_mode: 'pi pi-send',
-//   vibration: 'mdi mdi-vibrate',
-//   power_outage_count: 'mdi mdi-power-plug-off',
-//   angle_x: 'mdi mdi-axis-x-arrow',
-//   angle_y: 'mdi mdi-axis-y-arrow',
-//   angle_z: 'mdi mdi-axis-z-arrow',
-//   side: 'mdi mdi-cube-outline',
-//   presence: 'mdi mdi-account-check',
-//   contact: 'mdi mdi-door', // mdi-door-closed, mdi-door-open
-// };
-
 const sensorUnits: KeyValuePair<string> = {
   brightness: '%',
   temperature: '°C',
@@ -239,12 +197,12 @@ export function getSensorName(sensor: string): string {
   if (!sensor) {
     return 'Sensor not found';
   }
-  return sensor.charAt(0).toUpperCase() + sensor.slice(1); // TODO: load name  from resources file
+  return sensor.replace('_', ' ').charAt(0).toUpperCase() + sensor.slice(1);
 }
 
 export function getSensorValue(value: any): any {
   if (value == null) {
-    return '';
+    return '—';
   }
   if (typeof value == 'boolean' || typeof value == 'string') {
     return value;
