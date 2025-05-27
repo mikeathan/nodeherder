8fe60c440e115dd51fd359054b1d2cecb252608f
mikeathan <mikeathan@gmail.com>
2025-05-27 19:27:47 +0100

    add card edit mode style

--- frontend/src/components/dashboards/GroupDashboard.vue
+++ frontend/src/components/dashboards/GroupDashboard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { computed } from 'vue';
+  import { computed, ref } from 'vue';
   import { store } from '@/store';
   import { DashboardGroup, DashboardGroups, DeviceGroup } from '@/types/settings.type';
   import EntityCard from './cards/EntityCard.vue';
@@ -16,7 +16,7 @@
       }))
     );
   }
-
+  const isEditMode = ref(true);
   // to use for dynamically setting the column count
   //   :style="{ columnCount: calculateColumnCount(flattenDeviceGroup(group).length) }"
   // function calculateColumnCount(length: number): number {
@@ -30,6 +30,11 @@
     <div v-for="group in dashboardGroups" :key="group.name" class="dashboard-group">
       <h4 class="dashboard-title">{{ group.name }}</h4>
       <div class="card-container">
+        <div v-if="isEditMode" class="tools">
+          <span class="icon">⠿</span>
+          <span class="icon">✏️</span>
+          <span class="icon">🗑️</span>
+        </div>
         <div v-for="item in flattenDeviceGroup(group)" :key="`${item.deviceId}-${item.expose}`" class="card-item">
           <EntityCard :id="item.deviceId" :name="item.expose" compact />
         </div>
@@ -61,6 +66,11 @@
     column-gap: 0.5rem;
     max-width: 400px;
     /* margin: 0 auto; */
+    border: 2px dotted #d3d3d3;
+    border-radius: 12px;
+    padding: 16px;
+    position: relative;
+    margin-bottom: 20px;
   }
 
   .card-item {
@@ -68,5 +78,20 @@
     width: 100%;
     display: inline-block;
     break-inside: avoid;
+  }
+
+  .tools {
+    position: absolute;
+    top: -30px;
+    right: -10px;
+    padding: 4px 8px;
+    display: flex;
+    gap: 6px;
+    align-items: center;
+  }
+
+  .icon {
+    font-size: 14px;
+    cursor: pointer;
   }
 </style>
