323651772719761b009db038460bc8fb8fdfd1ec
mikeathan <mikeathan@gmail.com>
2025-07-17 19:30:06 +0100

    fix device override metricsenabled prop

--- frontend/src/contracts/settings.ts
+++ frontend/src/contracts/settings.ts
@@ -26,7 +26,7 @@ export function createDeviceConfigOverride(id: string): DeviceConfig {
   return {
     id: id,
     disabled: false,
-    history: false,
+    metricsEnabled: false,
     rateLimit: createTimeIntervalFromSeconds(60),
     defaultDebounceByCategory: {},
     debounceOverrides: {},
--- frontend/src/types/settings.type.ts
+++ frontend/src/types/settings.type.ts
@@ -42,7 +42,7 @@ export type DeviceSettings = {
 export type DeviceConfig = {
   id: string;
   disabled: boolean;
-  history: boolean;
+  metricsEnabled: boolean;
   rateLimit: TimeInterval;
   defaultDebounceByCategory: DeviceDebounce;
   debounceOverrides: DeviceDebounce;
