9e3b5f9fc11cf3cfca945e785524fde528a6328f
mikeathan <mikeathan@gmail.com>
2025-10-11 13:02:21 +0100

    wip

--- frontend/src/components/input/TimePicker.vue
+++ frontend/src/components/input/TimePicker.vue
@@ -1,72 +1,78 @@
 <script setup lang="ts">
-import { TimeValidationFunction } from '@/types/time-picker.types';
-import { computed, PropType, ref } from 'vue';
+  import { TimeValidationFunction } from '@/types/time-picker.types';
+  import { computed, PropType, ref } from 'vue';
 
-const props = defineProps({
-  value: {
-    type: Date,
-    default: '',
-    required: true,
-  },
-  label: {
-    type: String,
-    default: '',
-    required: false,
-  },
-  disabled: {
-    type: Boolean,
-    default: false,
-    required: false,
-  },
-  validation: {
-    type: Function as PropType<TimeValidationFunction>,
-    default: () => { },
-    required: false,
-  },
-});
+  const props = defineProps({
+    value: {
+      type: Date,
+      default: '',
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    validation: {
+      type: Function as PropType<TimeValidationFunction>,
+      default: () => {},
+      required: false,
+    },
+  });
 
-const selectedTime = ref<Date>(props.value);
-const errorMessage = ref<string>('');
-const hasError = computed(() => {
-  return errorMessage.value != '';
-});
+  const selectedTime = ref<Date>(props.value);
+  const errorMessage = ref<string>('');
+  const hasError = computed(() => {
+    return errorMessage.value != '';
+  });
 
-const setError = (message: string) => {
-  errorMessage.value = message;
-};
+  const setError = (message: string) => {
+    errorMessage.value = message;
+  };
 
-defineExpose({
-  setError,
-});
-const emit = defineEmits<{
-  (e: 'updated', value: any): void;
-}>();
+  defineExpose({
+    setError,
+  });
+  const emit = defineEmits<{
+    (e: 'updated', value: any): void;
+  }>();
 
-function handleEnterKey() {
-  if (props.validation) {
-    var res = props.validation(selectedTime.value);
-    if (res) {
-      setError(res);
-      return;
+  function handleEnterKey() {
+    if (props.validation) {
+      var res = props.validation(selectedTime.value);
+      if (res) {
+        setError(res);
+        return;
+      }
     }
+    setError('');
+    emit('updated', selectedTime.value);
   }
-  setError('');
-  emit('updated', selectedTime.value);
-}
 </script>
 <style scoped>
-.error-icon {
-  color: #F44336 !important;
-}
-
+  .error-icon {
+    color: #f44336 !important;
+  }
 </style>
 <template>
   <IftaLabel>
     <div class="date-picker-container" :class="{ 'has-error': hasError }">
-      <DatePicker inputId="date" id="datepicker-timeonly" v-model="selectedTime" showIcon fluid  iconDisplay="input"
-        timeOnly @blur="handleEnterKey"
-        @keyup.enter="handleEnterKey" 
-         :disabled="props.disabled" :class="{ 'error-overlay': hasError }">
+      <DatePicker
+        inputId="date"
+        id="datepicker-timeonly"
+        v-model="selectedTime"
+        showIcon
+        iconDisplay="input"
+        timeOnly
+        @blur="handleEnterKey"
+        @keyup.enter="handleEnterKey"
+        :disabled="props.disabled"
+        :class="{ 'error-overlay': hasError }">
         <template #inputicon="slotProps">
           <i class="pi pi-clock" @click="slotProps.clickCallback" :class="{ 'error-icon': hasError }" />
         </template>
