cda68f48a62396bbcaa5e44c018a7f262688ad74
mikeathan <mikeathan@gmail.com>
2025-10-18 14:40:29 +0100

    wip

--- frontend/src/components/auth/LoginPage.vue
+++ frontend/src/components/auth/LoginPage.vue
@@ -37,6 +37,54 @@
     color: red;
     font-size: 0.875rem;
   }
+
+  .google-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    font-weight: 500;
+    transition: background-color 0.2s, box-shadow 0.2s;
+    min-width: 200px;
+
+    background-color: #131314;
+    background-image: none;
+    border: 1px solid #747775;
+    -webkit-border-radius: 20px;
+    border-radius: 20px;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    color: #e3e3e3;
+    cursor: pointer;
+    font-family: 'Roboto', arial, sans-serif;
+    font-size: 14px;
+    height: 40px;
+    letter-spacing: 0.25px;
+    outline: none;
+    overflow: hidden;
+    padding: 0 12px;
+    position: relative;
+    text-align: center;
+  }
+
+  .google-button:hover {
+    box-shadow: 0 1px 2px rgba(60, 64, 67, 0.3), 0 1px 3px rgba(60, 64, 67, 0.15);
+  }
+
+  .google-button:active {
+    background-color: rgba(0, 0, 0, 0.08);
+  }
+
+  .google-button:disabled {
+    opacity: 0.5;
+    cursor: default;
+  }
+
+  .icon {
+    width: 20px;
+    height: 20px;
+  }
 </style>
 
 <!-- <template>
@@ -53,114 +101,12 @@
     <p v-if="error" class="error-message">{{ error }}</p>
   </div>
 </template> -->
-<style scoped>
-.gsi-material-button {
-  -moz-user-select: none;
-  -webkit-user-select: none;
-  -ms-user-select: none;
-  -webkit-appearance: none;
-  background-color: white;
-  border: 1px solid #747775;
-  border-radius: 4px;
-  box-sizing: border-box;
-  color: #1f1f1f;
-  cursor: pointer;
-  font-family: 'Roboto', arial, sans-serif;
-  font-size: 14px;
-  height: 40px;
-  letter-spacing: 0.25px;
-  outline: none;
-  overflow: hidden;
-  padding: 0 12px;
-  position: relative;
-  text-align: center;
-  transition: background-color .218s, border-color .218s, box-shadow .218s;
-  vertical-align: middle;
-  white-space: nowrap;
-  width: auto;
-  max-width: 400px;
-  min-width: min-content;
-  display: flex;
-  align-items: center;
-}
-
-.gsi-material-button .gsi-material-button-icon {
-  height: 20px;
-  margin-right: 12px;
-  width: 20px;
-}
-
-.gsi-material-button .gsi-material-button-content-wrapper {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  height: 100%;
-  width: 100%;
-}
-
-.gsi-material-button .gsi-material-button-contents {
-  flex-grow: 1;
-  font-weight: 500;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  vertical-align: top;
-}
-
-.gsi-material-button .gsi-material-button-state {
-  transition: opacity .218s;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  opacity: 0;
-}
-
-.gsi-material-button:disabled {
-  cursor: default;
-  background-color: #ffffff61;
-  border-color: #1f1f1f1f;
-}
-
-.gsi-material-button:disabled .gsi-material-button-contents,
-.gsi-material-button:disabled .gsi-material-button-icon {
-  opacity: 38%;
-}
-
-.gsi-material-button:not(:disabled):active .gsi-material-button-state,
-.gsi-material-button:not(:disabled):focus .gsi-material-button-state {
-  background-color: #303030;
-  opacity: 12%;
-}
-
-.gsi-material-button:not(:disabled):hover {
-  box-shadow: 0 1px 2px 0 rgba(60, 64, 67, .30), 0 1px 3px 1px rgba(60, 64, 67, .15);
-}
-
-.gsi-material-button:not(:disabled):hover .gsi-material-button-state {
-  background-color: #303030;
-  opacity: 8%;
-}
-</style>
-
 
 <template>
   <div class="login-wrapper flex flex-column align-items-center justify-content-center gap-3">
-    <button
-      @click="handleLogin"
-      :disabled="loading"
-      class="gsi-material-button flex align-items-center"
-    >
-      <div class="gsi-material-button-state"></div>
-      <div class="gsi-material-button-content-wrapper">
-        <div class="gsi-material-button-icon">
-          <GoogleIcon />
-        </div>
-        <span class="gsi-material-button-contents">
-          {{ loading ? 'Signing in...' : 'Sign in with Google' }}
-        </span>
-      </div>
+    <button class="google-button" :disabled="loading" @click="handleLogin">
+      <GoogleIcon class="icon" />
+      <span>{{ loading ? 'Signing in...' : 'Sign in with Google' }}</span>
     </button>
   </div>
 </template>
